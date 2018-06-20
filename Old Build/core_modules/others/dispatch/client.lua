83158dff29f04ba9037ccc95c0a28b9d49d03115
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2018-06-20 01:43:47 +0100

    Added call events, fixed ped ids

--- Old Build/core_modules/others/dispatch/client.lua
+++ Old Build/core_modules/others/dispatch/client.lua
@@ -422,3 +422,66 @@ AddEventHandler("dispatch:notify-cops31", function(message)
 	plate = string.gsub(plate, "VEHICLE PLATE", GetVehicleNumberPlateText(vehicle))
 	TriggerServerEVent("dispatch:notify-cops31", message, vehicle, plate)
 end)
+
+local user_call_311_timer = 10
+local user_call_911_timer = 10
+
+RegisterNetEvent("dispatch:311")
+AddEventHandler("dispatch:311", function(id, message)
+	local user_call_311 = nil
+	local id = GetPlayerPed(GetPlayerFromServerId(id))
+	local coords = GetEntityCoords(id, false)
+	local endTime = GetGameTimer() + ((user_call_311_timer * 60)/ 0.001)
+	local arrived = false
+	while endTime > GetGameTimer() and not arrived do
+		Citizen.Wait(0)
+		local pos = GetEntityCoords(PlayerPedId(), false)
+		if not DoesBlipExist(user_call_311) then
+			user_call_311 = AddBlipForCoord(coords.x, coords.y, coords.z)
+			SetBlipSprite(user_call_311, 459)
+			SetBlipColour(user_call_311, 28)
+			SetBlipAsShortRange(user_call_311, true)
+			SetBlipScale(user_call_311, 0.85)
+			BeginTextCommandSetBlipName("STRING")
+			AddTextComponentString("311 Call")
+			EndTextCommandSetBlipName(user_call_311)
+		end
+		if not arrived then
+			if GetDistanceBetweenCoords(pos.x, pos.y, pos.z, coords.x, coords.y, coords.z, true) < 20 then
+				arrived = true
+				TriggerServerEvent("dispatch:pay", "311")
+			end
+		end
+	end
+	RemoveBlip(user_call_311)
+end)
+
+RegisterNetEvent("dispatch:911")
+AddEventHandler("dispatch:911", function(id, message)
+	local user_call_911 = nil
+	local id = GetPlayerPed(GetPlayerFromServerId(id))
+	local coords = GetEntityCoords(id, false)
+	local endTime = GetGameTimer() + ((user_call_911_timer * 60)/ 0.001)
+	local arrived = false
+	while endTime > GetGameTimer() and not arrived do
+		Citizen.Wait(0)
+		local pos = GetEntityCoords(PlayerPedId(), false)
+		if not DoesBlipExist(user_call_911) then
+			user_call_911 = AddBlipForCoord(coords.x, coords.y, coords.z)
+			SetBlipSprite(user_call_911, 459)
+			SetBlipColour(user_call_911, 28)
+			SetBlipAsShortRange(user_call_911, true)
+			SetBlipScale(user_call_911, 0.85)
+			BeginTextCommandSetBlipName("STRING")
+			AddTextComponentString("911 Call")
+			EndTextCommandSetBlipName(user_call_911)
+		end
+		if not arrived then
+			if GetDistanceBetweenCoords(pos.x, pos.y, pos.z, coords.x, coords.y, coords.z, true) < 20 then
+				arrived = true
+				TriggerServerEvent("dispatch:pay", "911")
+			end
+		end
+	end
+	RemoveBlip(user_call_911)
+end)
