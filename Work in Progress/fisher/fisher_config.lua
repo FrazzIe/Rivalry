67055c55e4cdb86faabbf1e5dabc8c5c378dffc8
Ructar <duckhamm@gmail.com>
2018-06-27 19:29:18 -0400

    fisher wip

---
+++ Work in Progress/fisher/fisher_config.lua
@@ -0,0 +1,152 @@
+Fisher_platesuffix="Pech"
+
+Fisher_car = {
+	id=22,
+	x=3860.79663085938, 
+	y=4473.5048828125,
+	z=0.473495811223984
+}
+
+--{3792.85620117188, 4472.23828125,  5.20042514801025, 110.595985412598}
+
+Fisher_car2 = {
+	id=22,
+	x=3792.85620117188, 
+	y=4472.23828125,
+	z=5.20042514801025
+}
+
+Fisher_pay = {
+	minimum = 100,
+	maximum = 200,
+}
+
+Fisher_blips = {
+	["Entreprise"] = {
+		id=17,
+		x=3804.07275390625,
+		y=4476.6298828125,
+		z=4.9926872253418,
+		distanceBetweenCoords=2.5,
+		distanceMarker=2.5,
+		defaultTime=10,
+		name="Fisherman"
+	},
+	["Garage"] = {
+		id=18,
+		x=3865.51171875,
+		y=4464.83056640625,
+		z=1.76628189086914,
+		distanceBetweenCoords=15,
+		distanceMarker=15,
+		defaultTime=10,
+		name="Boat Yard"
+	},
+	["Fish Shop"] = {
+		id=20,
+		x=1301.46472167969,
+		y=4319.66650390625,
+		z=37.1105293273926,
+		distanceBetweenCoords=10,
+		distanceMarker=15,
+		defaultTime=5000,
+		name="Fish factory"
+	},
+	["Point of Sale"] = {
+		id=21,
+		x=-2196.14,
+		y=4267.99,
+		z=48.0065,
+		distanceBetweenCoords=10,
+		distanceMarker=15,
+		defaultTime=5000,
+		name="Sell fish"
+	},
+	["Deep Sea Zone"] = {
+		id=404,
+		x = 4155.1572265625, 
+		y = 4766.2548828125, 
+		z = 0.13745714724064,
+		distanceBetweenCoords=10,
+		distanceMarker=15,
+		defaultTime=5000,
+		name="Deep Sea Fish"
+	},
+	["Deep Sea Zone"] = {
+		id=404,
+		x = 4155.1572265625, 
+		y = 4766.2548828125, 
+		z = 0.13745714724064,
+		distanceBetweenCoords=10,
+		distanceMarker=15,
+		defaultTime=5000,
+		name="Deep Sea Fish"
+	},
+}
+
+Piers = {
+	[1] = {
+		id=22,
+		x = -1855.3083496094, 
+		y = -1246.1899414063,
+		z = 8.615777015686,
+		distanceBetweenCoords=3.5,
+		distanceMarker=3.5,
+		defaultTime=10000,
+		name="Pier Fishing"
+	},
+	[2] = {
+		id=23,
+		x = -279.33352661133, 
+		y = 6636.9775390625, 
+		z = 7.5514945983887,
+		distanceBetweenCoords=3.5,
+		distanceMarker=3.5,
+		defaultTime=10000,
+		name="Fishing docks"
+	},
+	[3] = {
+		id=24,
+		x = -3428.34375, 
+		y = 967.8837890625, 
+		z = 8.3466930389404,
+		distanceBetweenCoords=3.5,
+		distanceMarker=3.5,
+		defaultTime=10000,
+		name="Fishing docks"
+	},
+	[4] = {
+		id=25,
+		x = 1732.7165527344, 
+		y = 3985.6623535156, 
+		z = 31.978717803955,
+		distanceBetweenCoords=3.5,
+		distanceMarker=3.5,
+		defaultTime=10000,
+		name="Fishing docks"
+	},
+	[5] = {
+		id=26,
+		x = 3866.8852539063, 
+		y = 4463.630859375, 
+		z = 2.7258133888245,
+		distanceBetweenCoords=3.5,
+		distanceMarker=3.5,
+		defaultTime=10000,
+		name="Fishing docks"
+	},
+}
+
+DeepSeaFishing = {
+	[1] = {
+		id=27,
+		x = 4155.1572265625, 
+		y = 4766.2548828125, 
+		z = 0.13745714724064,
+		distanceBetweenCoords=20.5,
+		distanceMarker=20.5,
+		defaultTime=10000,
+		name="Zone Fishing"
+	},
+}
+
