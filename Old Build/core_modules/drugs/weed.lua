1906768b4a2904d186f7ab0946ea269bca080240
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2018-07-26 08:00:59 +0100

    slightly increased chances across the board to sell joints. made significant increase to chance to sell in certain bad areas of san andreas | disabled vehicle selling

--- Old Build/core_modules/drugs/weed.lua
+++ Old Build/core_modules/drugs/weed.lua
@@ -1,93 +1,93 @@
 local drug_zones = { 
-	['AIRP'] = {label = "Los Santos International Airport", chance = 50}, -- 80% chance of success for selling drugs to a civ
+	['AIRP'] = {label = "Los Santos International Airport", chance = 80}, -- 80% chance of success for selling drugs to a civ
 	['ALAMO'] = {label = "Alamo Sea", chance = 50}, 
-	['ALTA'] = {label = "Alta", chance = 50}, 
+	['ALTA'] = {label = "Alta", chance = 60}, 
 	['ARMYB'] = {label = "Fort Zancudo", chance = 0}, 
-	['BANHAMC'] = {label = "Banham Canyon Dr", chance = 50}, 
-	['BANNING'] = {label = "Banning", chance = 50}, 
-	['BEACH'] = {label = "Vespucci Beach", chance = 70}, 
-	['BHAMCA'] = {label = "Banham Canyon", chance = 50}, 
-	['BRADP'] = {label = "Braddock Pass", chance = 50}, 
-	['BRADT'] = {label = "Braddock Tunnel", chance = 50}, 
-	['BURTON'] = {label = "Burton", chance = 50}, 
-	['CALAFB'] = {label = "Calafia Bridge", chance = 50}, 
-	['CANNY'] = {label = "Raton Canyon", chance = 50}, 
-	['CCREAK'] = {label = "Cassidy Creek", chance = 50}, 
-	['CHAMH'] = {label = "Chamberlain Hills", chance = 50}, 
-	['CHIL'] = {label = "Vinewood Hills", chance = 50}, 
-	['CHU'] = {label = "Chumash", chance = 50}, 
-	['CMSW'] = {label = "Chiliad Mountain State Wilderness", chance = 70}, 
-	['CYPRE'] = {label = "Cypress Flats", chance = 50},
-	['DAVIS'] = {label = "Davis", chance = 70}, 
-	['DELBE'] = {label = "Del Perro Beach", chance = 70}, 
-	['DELPE'] = {label = "Del Perro", chance = 50}, 
-	['DELSOL'] = {label = "La Puerta", chance = 50}, 
-	['DESRT'] = {label = "Grand Senora Desert", chance = 50}, 
-	['DOWNT'] = {label = "Downtown", chance = 50}, 
-	['DTVINE'] = {label = "Downtown Vinewood", chance = 50}, 
-	['EAST_V'] = {label = "East Vinewood", chance = 50}, 
-	['EBURO'] = {label = "El Burro Heights", chance = 50}, 
-	['ELGORL'] = {label = "El Gordo Lighthouse", chance = 50}, 
-	['ELYSIAN'] = {label = "Elysian Island", chance = 50}, 
-	['GALFISH'] = {label = "Galilee", chance = 50}, 
-	['GOLF'] = {label = "GWC and Golfing Society", chance = 50}, 
-	['GRAPES'] = {label = "Grapeseed", chance = 70}, 
-	['GREATC'] = {label = "Great Chaparral", chance = 50}, 
-	['HARMO'] = {label = "Harmony", chance = 50}, 
-	['HAWICK'] = {label = "Hawick", chance = 50}, 
-	['HORS'] = {label = "Vinewood Racetrack", chance = 50}, 
+	['BANHAMC'] = {label = "Banham Canyon Dr", chance = 60}, 
+	['BANNING'] = {label = "Banning", chance = 60}, 
+	['BEACH'] = {label = "Vespucci Beach", chance = 80}, 
+	['BHAMCA'] = {label = "Banham Canyon", chance = 60}, 
+	['BRADP'] = {label = "Braddock Pass", chance = 60}, 
+	['BRADT'] = {label = "Braddock Tunnel", chance = 60}, 
+	['BURTON'] = {label = "Burton", chance = 60}, 
+	['CALAFB'] = {label = "Calafia Bridge", chance = 60}, 
+	['CANNY'] = {label = "Raton Canyon", chance = 60}, 
+	['CCREAK'] = {label = "Cassidy Creek", chance = 60}, 
+	['CHAMH'] = {label = "Chamberlain Hills", chance = 60}, 
+	['CHIL'] = {label = "Vinewood Hills", chance = 60}, 
+	['CHU'] = {label = "Chumash", chance = 60}, 
+	['CMSW'] = {label = "Chiliad Mountain State Wilderness", chance = 80}, 
+	['CYPRE'] = {label = "Cypress Flats", chance = 60},
+	['DAVIS'] = {label = "Davis", chance = 80}, 
+	['DELBE'] = {label = "Del Perro Beach", chance = 80}, 
+	['DELPE'] = {label = "Del Perro", chance = 60}, 
+	['DELSOL'] = {label = "La Puerta", chance = 60}, 
+	['DESRT'] = {label = "Grand Senora Desert", chance = 60}, 
+	['DOWNT'] = {label = "Downtown", chance = 60}, 
+	['DTVINE'] = {label = "Downtown Vinewood", chance = 60}, 
+	['EAST_V'] = {label = "East Vinewood", chance = 60}, 
+	['EBURO'] = {label = "El Burro Heights", chance = 70}, 
+	['ELGORL'] = {label = "El Gordo Lighthouse", chance = 60}, 
+	['ELYSIAN'] = {label = "Elysian Island", chance = 60}, 
+	['GALFISH'] = {label = "Galilee", chance = 60}, 
+	['GOLF'] = {label = "GWC and Golfing Society", chance = 60}, 
+	['GRAPES'] = {label = "Grapeseed", chance = 80}, 
+	['GREATC'] = {label = "Great Chaparral", chance = 60}, 
+	['HARMO'] = {label = "Harmony", chance = 60}, 
+	['HAWICK'] = {label = "Hawick", chance = 60}, 
+	['HORS'] = {label = "Vinewood Racetrack", chance = 60}, 
 	['HUMLAB'] = {label = "Humane Labs and Research", chance = 0}, 
 	['JAIL'] = {label = "Bolingbroke Penitentiary", chance = 0}, 
-	['KOREAT'] = {label = "Little Seoul", chance = 70}, 
-	['LACT'] = {label = "Land Act Reservoir", chance = 50}, 
-	['LAGO'] = {label = "Lago Zancudo", chance = 50}, 
-	['LDAM'] = {label = "Land Act Dam", chance = 50}, 
-	['LEGSQU'] = {label = "Legion Square", chance = 50}, 
-	['LMESA'] = {label = "La Mesa", chance = 50}, 
-	['LOSPUER'] = {label = "La Puerta", chance = 50}, 
-	['MIRR'] = {label = "Mirror Park", chance = 70}, 
-	['MORN'] = {label = "Morningwood", chance = 50}, 
-	['MOVIE'] = {label = "Richards Majestic", chance = 50}, 
+	['KOREAT'] = {label = "Little Seoul", chance = 80}, 
+	['LACT'] = {label = "Land Act Reservoir", chance = 60}, 
+	['LAGO'] = {label = "Lago Zancudo", chance = 60}, 
+	['LDAM'] = {label = "Land Act Dam", chance = 60}, 
+	['LEGSQU'] = {label = "Legion Square", chance = 60}, 
+	['LMESA'] = {label = "La Mesa", chance = 70}, 
+	['LOSPUER'] = {label = "La Puerta", chance = 60}, 
+	['MIRR'] = {label = "Mirror Park", chance = 80}, 
+	['MORN'] = {label = "Morningwood", chance = 60}, 
+	['MOVIE'] = {label = "Richards Majestic", chance = 60}, 
 	['MTCHIL'] = {label = "Mount Chiliad", chance = 70}, 
 	['MTGORDO'] = {label = "Mount Gordo", chance = 70}, 
 	['MTJOSE'] = {label = "Mount Josiah", chance = 70}, 
-	['MURRI'] = {label = "Murrieta Heights", chance = 70}, 
-	['NCHU'] = {label = "North Chumash", chance = 50}, 
+	['MURRI'] = {label = "Murrieta Heights", chance = 60}, 
+	['NCHU'] = {label = "North Chumash", chance = 60}, 
 	['NOOSE'] = {label = "N.O.O.S.E", chance = 0}, 
-	['OCEANA'] = {label = "Pacific Ocean", chance = 50}, 
-	['PALCOV'] = {label = "Paleto Cove", chance = 50}, 
-	['PALETO'] = {label = "Paleto Bay", chance = 50}, 
-	['PALFOR'] = {label = "Paleto Forest", chance = 50}, 
-	['PALHIGH'] = {label = "Palomino Highlands", chance = 50}, 
-	['PALMPOW'] = {label = "Palmer-Taylor Power Station", chance = 50}, 
-	['PBLUFF'] = {label = "Pacific Bluffs", chance = 50}, 
-	['PBOX'] = {label = "Pillbox Hill", chance = 50}, 
-	['PROCOB'] = {label = "Procopio Beach", chance = 50}, 
+	['OCEANA'] = {label = "Pacific Ocean", chance = 60}, 
+	['PALCOV'] = {label = "Paleto Cove", chance = 60}, 
+	['PALETO'] = {label = "Paleto Bay", chance = 70}, 
+	['PALFOR'] = {label = "Paleto Forest", chance = 60}, 
+	['PALHIGH'] = {label = "Palomino Highlands", chance = 60}, 
+	['PALMPOW'] = {label = "Palmer-Taylor Power Station", chance = 60}, 
+	['PBLUFF'] = {label = "Pacific Bluffs", chance = 60}, 
+	['PBOX'] = {label = "Pillbox Hill", chance = 60}, 
+	['PROCOB'] = {label = "Procopio Beach", chance = 60}, 
 	['RANCHO'] = {label = "Rancho", chance = 70}, 
-	['RGLEN'] = {label = "Richman Glen", chance = 50}, 
-	['RICHM'] = {label = "Richman", chance = 50}, 
-	['ROCKF'] = {label = "Rockford Hills", chance = 50}, 
-	['RTRAK'] = {label = "Redwood Lights Track", chance = 50}, 
-	['SANAND'] = {label = "San Andreas", chance = 50}, 
-	['SANCHIA'] = {label = "San Chianski Mountain Range", chance = 50}, 
-	['SANDY'] = {label = "Sandy Shores", chance = 50}, 
-	['SKID'] = {label = "Mission Row", chance = 50}, 
-	['SLAB'] = {label = "Stab City", chance = 70}, 
-	['STAD'] = {label = "Maze Bank Arena", chance = 50}, 
-	['STRAW'] = {label = "Strawberry", chance = 70}, 
-	['TATAMO'] = {label = "Tataviam Mountains", chance = 50}, 
-	['TERMINA'] = {label = "Terminal", chance = 50}, 
-	['TEXTI'] = {label = "Textile City", chance = 50}, 
-	['TONGVAH'] = {label = "Tongva Hills", chance = 50}, 
-	['TONGVAV'] = {label = "Tongva Valley", chance = 50}, 
-	['VCANA'] = {label = "Vespucci Canals", chance = 50}, 
-	['VESP'] = {label = "Vespucci", chance = 50}, 
-	['VINE'] = {label = "Vinewood", chance = 50}, 
-	['WINDF'] = {label = "Ron Alternates Wind Farm", chance = 70}, 
-	['WVINE'] = {label = "West Vinewood", chance = 50}, 
-	['ZANCUDO'] = {label = "Zancudo River", chance = 50}, 
-	['ZP_ORT'] = {label = "Port of South Los Santos", chance = 50}, 
-	['ZQ_UAR'] = {label = "Davis Quartz", chance = 50},
+	['RGLEN'] = {label = "Richman Glen", chance = 60}, 
+	['RICHM'] = {label = "Richman", chance = 60}, 
+	['ROCKF'] = {label = "Rockford Hills", chance = 60}, 
+	['RTRAK'] = {label = "Redwood Lights Track", chance = 60}, 
+	['SANAND'] = {label = "San Andreas", chance = 60}, 
+	['SANCHIA'] = {label = "San Chianski Mountain Range", chance = 60}, 
+	['SANDY'] = {label = "Sandy Shores", chance = 75}, 
+	['SKID'] = {label = "Mission Row", chance = 60}, 
+	['SLAB'] = {label = "Stab City", chance = 75}, 
+	['STAD'] = {label = "Maze Bank Arena", chance = 60}, 
+	['STRAW'] = {label = "Strawberry", chance = 80}, 
+	['TATAMO'] = {label = "Tataviam Mountains", chance = 60}, 
+	['TERMINA'] = {label = "Terminal", chance = 60}, 
+	['TEXTI'] = {label = "Textile City", chance = 60}, 
+	['TONGVAH'] = {label = "Tongva Hills", chance = 60}, 
+	['TONGVAV'] = {label = "Tongva Valley", chance = 60}, 
+	['VCANA'] = {label = "Vespucci Canals", chance = 60}, 
+	['VESP'] = {label = "Vespucci", chance = 60}, 
+	['VINE'] = {label = "Vinewood", chance = 60}, 
+	['WINDF'] = {label = "Ron Alternates Wind Farm", chance = 60}, 
+	['WVINE'] = {label = "West Vinewood", chance = 60}, 
+	['ZANCUDO'] = {label = "Zancudo River", chance = 60}, 
+	['ZP_ORT'] = {label = "Port of South Los Santos", chance = 60}, 
+	['ZQ_UAR'] = {label = "Davis Quartz", chance = 60},
 }
 local joint_id, weed_id = 8, 4
 local weed_pay = {
@@ -315,66 +315,68 @@ Citizen.CreateThread(function()
 	end
 	while true do
 		Citizen.Wait(0)
-		if GetItemQuantity(joint_id) > 0 then
-			for ped in EnumeratePeds() do
-				if DoesEntityExist(ped) then
-					if not IsEntityDead(ped) then
-						if not IsPedAPlayer(ped) and not DecorGetBool(ped, "isTrader") and not DecorGetBool(ped, "soldTo") and not IsPedAnAnimal(GetEntityModel(ped)) then
-							if Vdist2(GetEntityCoords(PlayerPedId(), false), GetEntityCoords(ped, false)) < 1 then
-								if sell_cooldown > 0 then
-									DisplayHelpText("Press ~INPUT_CONTEXT~ to sell a joint! ["..sell_cooldown.."]")
-								else
-									DisplayHelpText("Press ~INPUT_CONTEXT~ to sell a joint!")
-									if IsControlJustPressed(1, 51) then
-										sell_cooldown = 10
-										DecorSetBool(ped, "soldTo", true)
-										if willNPCbuy() then
+		if not IsPedSittingInAnyVehicle(PlayerPedId()) then
+			if GetItemQuantity(joint_id) > 0 then
+				for ped in EnumeratePeds() do
+					if DoesEntityExist(ped) then
+						if not IsEntityDead(ped) then
+							if not IsPedAPlayer(ped) and not DecorGetBool(ped, "isTrader") and not DecorGetBool(ped, "soldTo") and not IsPedAnAnimal(GetEntityModel(ped)) then
+								if Vdist2(GetEntityCoords(PlayerPedId(), false), GetEntityCoords(ped, false)) < 1 then
+									if sell_cooldown > 0 then
+										DisplayHelpText("Press ~INPUT_CONTEXT~ to sell a joint! ["..sell_cooldown.."]")
+									else
+										DisplayHelpText("Press ~INPUT_CONTEXT~ to sell a joint!")
+										if IsControlJustPressed(1, 51) then
+											sell_cooldown = 10
+											DecorSetBool(ped, "soldTo", true)
+											if willNPCbuy() then
 
-											local methbag_model = GetHashKey("prop_meth_bag_01")
-											
-											RequestModel(methbag_model)
+												local methbag_model = GetHashKey("prop_meth_bag_01")
+												
+												RequestModel(methbag_model)
 
-											while not HasModelLoaded(methbag_model) do
-												Citizen.Wait(250)
-											end
+												while not HasModelLoaded(methbag_model) do
+													Citizen.Wait(250)
+												end
 
-											SetEntityHeading(ped, GetEntityHeading(PlayerPedId()) - 180)
+												SetEntityHeading(ped, GetEntityHeading(PlayerPedId()) - 180)
 
-											local methbag = CreateObject(methbag_model, 0.01, 0, 0, 1, 0, 0)
+												local methbag = CreateObject(methbag_model, 0.01, 0, 0, 1, 0, 0)
 
-											AttachEntityToEntity(methbag, PlayerPedId(), GetPedBoneIndex(GetPlayerPed(-1), 64096), 0.0, 0.0, 0.020, 90.0, -10.0, -130.0 ,true, true, false, true, 1, true)
+												AttachEntityToEntity(methbag, PlayerPedId(), GetPedBoneIndex(GetPlayerPed(-1), 64096), 0.0, 0.0, 0.020, 90.0, -10.0, -130.0 ,true, true, false, true, 1, true)
 
-											TaskPlayAnim(PlayerPedId(), "mp_common", "givetake1_a", 100.0, 200.0, 0.3, 16, 0.2, 0, 0, 0)
+												TaskPlayAnim(PlayerPedId(), "mp_common", "givetake1_a", 100.0, 200.0, 0.3, 16, 0.2, 0, 0, 0)
 
-	                    					TaskPlayAnim(ped, "mp_common", "givetake1_b", 100.0, 200.0, 0.3, 16, 0.2, 0, 0, 0)
+		                    					TaskPlayAnim(ped, "mp_common", "givetake1_b", 100.0, 200.0, 0.3, 16, 0.2, 0, 0, 0)
 
-	                    					Citizen.Wait(500)
+		                    					Citizen.Wait(500)
 
-	                    					DestroyObject(methbag)
+		                    					DestroyObject(methbag)
 
-											local amount = GetRandomIntInRange(1, 8)
+												local amount = GetRandomIntInRange(1, 8)
 
-											if GetItemQuantity(joint_id) >= amount then
-												removeQty(joint_id, amount)
-												local pay = GetRandomIntInRange(weed_pay.min, weed_pay.max+1) * amount
-												local money_type = GetRandomIntInRange(1, 3)
-												if getCops() <= 0 then pay = pay/2 end
-												Notify("You sold "..amount.." joint(s) for <span style='color:lime'>$</span><span style='color:white'>"..math.floor(pay).."</span>")
-												TriggerServerEvent("weed:sell", math.floor(pay), money_type)
+												if GetItemQuantity(joint_id) >= amount then
+													removeQty(joint_id, amount)
+													local pay = GetRandomIntInRange(weed_pay.min, weed_pay.max+1) * amount
+													local money_type = GetRandomIntInRange(1, 3)
+													if getCops() <= 0 then pay = pay/2 end
+													Notify("You sold "..amount.." joint(s) for <span style='color:lime'>$</span><span style='color:white'>"..math.floor(pay).."</span>")
+													TriggerServerEvent("weed:sell", math.floor(pay), money_type)
+												else
+													removeQty(joint_id, 1)
+													local pay = GetRandomIntInRange(weed_pay.min, weed_pay.max+1)
+													local money_type = GetRandomIntInRange(1, 3)
+													if getCops() <= 0 then pay = pay/2 end
+													Notify("You sold "..amount.." joint(s) for <span style='color:lime'>$</span><span style='color:white'>"..math.floor(pay).."</span>")
+													TriggerServerEvent("weed:sell", math.floor(pay), money_type)
+												end
+
+												PlaySoundFrontend(-1, "Hack_Success", "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS", true)
 											else
-												removeQty(joint_id, 1)
-												local pay = GetRandomIntInRange(weed_pay.min, weed_pay.max+1)
-												local money_type = GetRandomIntInRange(1, 3)
-												if getCops() <= 0 then pay = pay/2 end
-												Notify("You sold "..amount.." joint(s) for <span style='color:lime'>$</span><span style='color:white'>"..math.floor(pay).."</span>")
-												TriggerServerEvent("weed:sell", math.floor(pay), money_type)
+												TaskReactAndFleePed(ped, PlayerPedId())
+												PlaySoundFrontend(-1, "Hack_Failed", "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS", true)
+												TriggerEvent("dispatch:drug")
 											end
-
-											PlaySoundFrontend(-1, "Hack_Success", "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS", true)
-										else
-											TaskReactAndFleePed(ped, PlayerPedId())
-											PlaySoundFrontend(-1, "Hack_Failed", "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS", true)
-											TriggerEvent("dispatch:drug")
 										end
 									end
 								end
