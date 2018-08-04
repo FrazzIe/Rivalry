0537e035ae8812214aff3dae3c9a571f6ca15756
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2018-08-04 23:52:43 +0100

    Updated identifier functions

--- New Build/Rivalry/Utilities/Server/Functions.lua
+++ New Build/Rivalry/Utilities/Server/Functions.lua
@@ -1,57 +1,77 @@
 function Utilities.GetPlayerSteam(SourcePlayer)
-    for Index = 0, GetNumPlayerIdentifiers(SourcePlayer) do
-        local Identifier = GetPlayerIdentifier(SourcePlayer, Index)
-        
-        if string.sub(tostring(Identifier), 1, string.len("steam:")) == "steam:" then
-            return Identifier
+    if Core.Players[SourcePlayer] and Core.Players[SourcePlayer].Users and Core.Players[SourcePlayer].Users.Steam then
+        return Core.Players[SourcePlayer].Users.Steam
+    else
+        for Index = 0, GetNumPlayerIdentifiers(SourcePlayer) do
+            local Identifier = GetPlayerIdentifier(SourcePlayer, Index)
+            
+            if string.sub(tostring(Identifier), 1, string.len("steam:")) == "steam:" then
+                return Identifier
+            end
         end
-    end
 
-    return nil
+        return nil
+    end
 end
 
 function Utilities.GetPlayerSteam64(SourcePlayer)
-    local SteamHex = Utilities.GetPlayerSteam(SourcePlayer)
-    if SteamHex then
-        return math.floor(tonumber(string.sub(SteamHex, 7, string.len(SteamHex)), 16))
+    if Core.Players[SourcePlayer] and Core.Players[SourcePlayer].Users and Core.Players[SourcePlayer].Users.Steam64 then
+        return Core.Players[SourcePlayer].Users.Steam64
     else
-        return nil
-    end 
+        local SteamHex = Utilities.GetPlayerSteam(SourcePlayer)
+        if SteamHex then
+            return math.floor(tonumber(string.sub(SteamHex, 7, string.len(SteamHex)), 16))
+        else
+            return nil
+        end
+    end
 end
 
 function Utilities.GetPlayerSteam32(SourcePlayer)
-    local Steam64 = Utilities.GetPlayerSteam64(SourcePlayer)
-    if Steam64 then
-        Steam64 = tonumber(string.sub(Steam64, 2))
-        
-        local Y = Steam64 % 2 == 0 and 0 or 1
-        local Z = math.floor(math.abs(6561197960265728 - Steam64 - Y) / 2)
-        return "STEAM_0:"..Y..":"..(Y == 1 and Z -1 or Z)
+    if Core.Players[SourcePlayer] and Core.Players[SourcePlayer].Users and Core.Players[SourcePlayer].Users.Steam32 then
+        return Core.Players[SourcePlayer].Users.Steam32
     else
-        return nil
+        local Steam64 = Utilities.GetPlayerSteam64(SourcePlayer)
+        if Steam64 then
+            Steam64 = tonumber(string.sub(Steam64, 2))
+            
+            local Y = Steam64 % 2 == 0 and 0 or 1
+            local Z = math.floor(math.abs(6561197960265728 - Steam64 - Y) / 2)
+            return "STEAM_0:"..Y..":"..(Y == 1 and Z -1 or Z)
+        else
+            return nil
+        end
     end
 end
 
 function Utilities.GetPlayerLicense(SourcePlayer)
-    for Index = 0, GetNumPlayerIdentifiers(SourcePlayer) do
-        local Identifier = GetPlayerIdentifier(SourcePlayer, Index)
+    if Core.Players[SourcePlayer] and Core.Players[SourcePlayer].Users and Core.Players[SourcePlayer].Users.License then
+        return Core.Players[SourcePlayer].Users.License
+    else
+        for Index = 0, GetNumPlayerIdentifiers(SourcePlayer) do
+            local Identifier = GetPlayerIdentifier(SourcePlayer, Index)
 
-        if string.sub(tostring(Identifier), 1,  string.len("license:")) == "license:" then
-            return Identifier
+            if string.sub(tostring(Identifier), 1,  string.len("license:")) == "license:" then
+                return Identifier
+            end
         end
-    end
 
-    return nil
+        return nil
+    end
 end
 
 function Utilities.GetPlayerIP(SourcePlayer)
-    for Index = 0, GetNumPlayerIdentifiers(SourcePlayer) do
-        local Identifier = GetPlayerIdentifier(SourcePlayer, Index)
+    if Core.Players[SourcePlayer] and Core.Players[SourcePlayer].Users and Core.Players[SourcePlayer].Users.IP then
+        return Core.Players[SourcePlayer].Users.IP
+    else
+        for Index = 0, GetNumPlayerIdentifiers(SourcePlayer) do
+            local Identifier = GetPlayerIdentifier(SourcePlayer, Index)
 
-        if string.sub(tostring(v), 1, string.len("ip:")) == "ip:" then
-            return Identifier
+            if string.sub(tostring(v), 1, string.len("ip:")) == "ip:" then
+                return Identifier
+            end
         end
-    end
 
-    return GetPlayerEndpoint(SourcePlayer) or nil
+        return GetPlayerEndpoint(SourcePlayer) or nil
+    end
 end
