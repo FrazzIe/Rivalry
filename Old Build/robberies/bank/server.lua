b4050159482d7f8a3a7d4a11aa3814f0195c66c9
skinner904 <skinner904@gmail.com>
2018-08-10 11:38:11 -0400

    added pacific standard

--- Old Build/robberies/bank/server.lua
+++ Old Build/robberies/bank/server.lua
@@ -15,16 +15,23 @@ local robbers = {}
 local banks = {
 	["fleeca2"] = {
 		position = { ['x'] = -2957.6674804688, ['y'] = 481.45776367188, ['z'] = 15.697026252747 },
-		reward = math.random(25000,50000),
+		reward = math.random(55000,75000),
 		nameofbank = "Fleeca Bank (Highway)",
 		time = 300,
 		lastrobbed = 0,
 	},
 	["blainecounty"] = {
 		position = { ['x'] = -107.06505584717, ['y'] = 6474.8012695313, ['z'] = 31.62670135498 },
-		reward = math.random(15000,40000),
+		reward = math.random(35000,45000),
 		nameofbank = "Blaine County Savings",
 		time = 300,
+		lastrobbed = 0,
+	},
+	["pacificstandard"] = {
+		position = { ['x'] = 259.43179321289, ['y'] = 225.69024658203, ['z'] = 101.68327331543 },
+		reward = math.random(80000,110000),
+		nameofbank = "Pacific Standard Bank (Code Red)",
+		time = 420,
 		lastrobbed = 0,
 	}
 }
