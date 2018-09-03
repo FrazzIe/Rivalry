593a41ce12735e8e9723c78ca84e58a2b65c3b15
Ructar <duckhamm@gmail.com>
2018-09-03 18:54:30 -0400

    laundering money with gopostal van

--- Old Build/others/laundering/server.lua
+++ Old Build/others/laundering/server.lua
@@ -1,13 +1,25 @@
-
 RegisterServerEvent("dm:clean")
-AddEventHandler("dm:clean", function()
+AddEventHandler("dm:clean", function(distance)
 	local source = source
+	local payment = 0
 	TriggerEvent("core:getuser", source, function(user)
 		local dirty = user.get("dirty")
-		if dirty > 0 then
+		if distance < 1500 then
+			payment = 4000
+		elseif distance > 1500 and distance < 3000 then
+			payment = 8000
+		else
+			payment = 12000
+		end
+		if dirty >= payment and dirty > 0 then
+			user.addWallet(payment)
+			user.removeDirty(payment)
+			TriggerClientEvent("customNotification", source, "Money laundered!")
+		elseif dirty < 0 then
+			TriggerClientEvent("customNotification", source, "You have no more money to clean!")
+		else
 			user.addWallet(dirty)
 			user.removeDirty(dirty)
-			TriggerClientEvent("customNotification", source, "Money laundered")
 		end
 	end)
 end)
