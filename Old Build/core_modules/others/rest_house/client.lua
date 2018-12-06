c5624fc14e56a58db20538c8b5b00a65646f1d7d
Ructar <duckhamm@gmail.com>
2018-12-06 00:20:50 -0500

    Rest House WIP

--- Old Build/core_modules/others/rest_house/client.lua
+++ Old Build/core_modules/others/rest_house/client.lua
@@ -1,4 +1,14 @@
-local rest_house_markers = {}
+local rest_house_markers = {
+	[1] = {x = 1543.9691162109, y = 3591.2551269531, z = 35.452556610107, h = 302.49542236328},
+	[2] = {x = -1132.3039550781, y = 4952.8984375, z = 222.2686920166, h = 158.55874633789},
+	[3] = {x = 149.99578857422, y = -2203.3139648438, z = 4.6880264282227, h = 112.83338165283},
+}
+local allowed = false
+
+RegisterNetEvent('resthouse:approve')
+AddEventHandler('reshhouse:approve', function()
+	allowed = not allowed
+end)
 
 Citizen.CreateThread(function()
 	while true do
@@ -9,9 +19,19 @@ Citizen.CreateThread(function()
 			if Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 20 then
 				RenderMarker(v.x, v.y, v.z)
 				if Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 2 then
+					DisplayHelpText("Press ~INPUT_CONTEXT~ to recieve medical treatment!($3000)")
 					if IsControlJustPressed(1, 51) then
 						if IsEntityDead(PlayerPed) then
 							TriggerServerEvent("resthouse:pay")
+							if allowed then
+								Notify("You are currently being provided medical treatment", 30000)
+								Citizen.Wait(30000)
+								TriggerServerEvent("paramedic:revive", GetPlayerServerId(PlayerId()))
+								allowed = false
+							end
+						else
+							Notify("You look fine to me, go bother someone else!", 2500)
+							allowed = false
 						end
 					end
 				end
--- Old Build/core_modules/others/rest_house/server.lua
+++ Old Build/core_modules/others/rest_house/server.lua
@@ -2,9 +2,9 @@ RegisterServerEvent("resthouse:pay")
 AddEventHandler("resthouse:pay", function()
 	local source = source
 	TriggerEvent("core:getuser", source, function(user)
-		if user.get("wallet") >= 10000 then
-			user.removeWallet(10000)
-			TriggerClientEvent("paramedic:lifesupport", source, 300)
+		if user.get("wallet") >= 3000 then
+			user.removeWallet(3000)
+			TriggerClientEvent("resthouse:approve", source)
 		else
 			Notify("Insufficient funds!", 3000, source)
 		end
