5f7a404427e57fb968c9144eabda166a725d4dfb
awasur04 <awasur04@yahoo.com>
2018-12-28 21:11:21 -0600

    Changed Weather

--- Old Build/core_modules/others/environment/config.lua
+++ Old Build/core_modules/others/environment/config.lua
@@ -1,9 +1,11 @@
 Time, Weather = {}, {}
 Time.Hour, Time.Minute, Time.Second = 12, 0, 0
 Weather.Current, Weather.Types = "CLOUDS", {
-    "CLOUDS",
-    "OVERCAST",
-    "SNOWLIGHT",
+    "SUNNY",
+    "EXTRASUNNY", 
+    "CLEAR",  
+    "OVERCAST", 
+    "CLOUDS", 
 }
 
 --[[Weather.Current, Weather.Types = "SUNNY", {
