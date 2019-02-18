eb13b66bad9eff0c93bf49bf87715dcfb51da276
skinner904 <skinner904@gmail.com>
2019-02-18 17:23:14 -0500

    hunting buff

--- Old Build/jobs/hunting/client.lua
+++ Old Build/jobs/hunting/client.lua
@@ -87,7 +87,7 @@ AddEventHandler('hunting:drawSellViande', function (qte)
 		end
 		if qte > 0 then
 			TriggerEvent('inventory:removeQty',23, 1)
-			local salaire = math.random(30, 40)
+			local salaire = math.random(150, 300)
 			TriggerServerEvent('mission:completed', salaire)
 			ClearPrints()
 			SetTextEntry_2("STRING")
