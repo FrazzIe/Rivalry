eaa15c904ce3ef09627beb951c70a5d3a3471197
Ructar <duckhamm@gmail.com>
2019-02-25 00:03:25 -0500

    RenderTarget for TV

--- Old Build/core_modules/New/Player/Client/Main.lua
+++ Old Build/core_modules/New/Player/Client/Main.lua
@@ -92,6 +92,7 @@ Citizen.CreateThread(function()
 		end
 	end
 end)--]]
+
 local binoculars = false
 local fov_max = 70.0
 local fov_min = 5.0
@@ -159,6 +160,42 @@ function HandleZoom(cam)
 		SetCamFov(cam, current_fov + (fov - current_fov)*0.05)
 	end
 end
+
+function CreateNamedRenderTargetForModel(name, model)
+	local handle = 0
+	if not IsNamedRendertargetRegistered(name) then
+		RegisterNamedRendertarget(name, 0)
+	end
+	if not IsNamedRendertargetLinked(model) then
+		LinkNamedRendertarget(model)
+	end
+	if IsNamedRendertargetRegistered(name) then
+		handle = GetNamedRendertargetRenderId(name)
+	end
+	return handle
+end
+
+Citizen.CreateThread(function ()
+	local model = GetHashKey("ba_prop_club_screens_01")
+	local pos = { x = -1604.664, y = -3012.583, z = 80.00 }
+	local entity = GetClosestObjectOfType(pos.x, pos.y, pos.z, 0.05, model, 0, 0, 0)
+	local handle = CreateNamedRenderTargetForModel("club_projector", model)
+	RegisterScriptWithAudio(0)
+	SetTvChannel(-1)
+	Citizen.InvokeNative(0x9DD5A62390C3B735, 2, "PL_TOU_LSER_GALAXY", 0)
+	SetTvChannel(2)
+	EnableMovieSubtitles(1)
+	while true do
+	    SetTvAudioFrontend(0)
+	    AttachTvAudioToEntity(entity)
+	    SetTextRenderId(handle)
+	    Set_2dLayer(4)
+	    Citizen.InvokeNative(0xC6372ECD45D73BCD, 1)
+	    DrawTvChannel(0.5, 0.5, 1.0, 1.0, 0.0, 255, 255, 255, 255)
+	    SetTextRenderId(GetDefaultScriptRendertargetRenderId())
+	    Citizen.Wait(0)
+	end
+end)
 
 RegisterNetEvent('toggle:binoculars')
 AddEventHandler('toggle:binoculars', function()
