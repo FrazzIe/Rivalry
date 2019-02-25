afe1b890ca47ee9861f182033cb1238d3c5e51cc
Ructar <duckhamm@gmail.com>
2019-02-25 01:06:12 -0500

    Noob

--- Old Build/others/paycheck/paycheck_cl.lua
+++ Old Build/others/paycheck/paycheck_cl.lua
@@ -1,14 +1,14 @@
 Citizen.CreateThread(function ()
 	while true do
 	Citizen.Wait(1440000)
-		if export.policejob:getIsInService() then
-			PoliceRank = export.policejob:getPoliceRank()
+		if exports.policejob:getIsInService() then
+			PoliceRank = exports.policejob:getPoliceRank()
 			TriggerServerEvent('paycheck:salary', PoliceRank)
-		elseif export.emsjob:getIsInService() then
-			EmsRank = export.emsjob:getParamedicRank()
+		elseif exports.emsjob:getIsInService() then
+			EmsRank = exports.emsjob:getParamedicRank()
 			TriggerServerEvent('paycheck:salary', EmsRank)
-		elseif export.core_modules:DOJisdoj() then
-			DojRank = export.core_modules:DOJgetrank()
+		elseif exports.core_modules:DOJisdoj() then
+			DojRank = exports.core_modules:DOJgetrank()
 			TriggerServerEvent('paycheck:salary', DojRank)
 		else
 			TriggerServerEvent('paycheck:salary')
