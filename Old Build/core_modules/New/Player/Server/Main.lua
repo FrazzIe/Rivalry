121e8d8371c36e51550bd91e7caf9adc47952c7e
skinner904 <skinner904@gmail.com>
2019-02-26 14:54:34 -0500

    increased armed robberies

--- Old Build/core_modules/New/Player/Server/Main.lua
+++ Old Build/core_modules/New/Player/Server/Main.lua
@@ -2,7 +2,7 @@ RegisterServerEvent("Rob:Sucessful")
 AddEventHandler("Rob:Sucessful", function(IsCops)
 	local Source = source
 	TriggerEvent("core:getuser", Source, function(User)
-		local RandomAmount = math.random(50, 150)
+		local RandomAmount = math.random(250, 750)
 		if IsCops then
 			User.addWallet(RandomAmount)
 		else
