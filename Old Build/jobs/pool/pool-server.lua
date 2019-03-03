1e7f419515dda92280f38ba08bfd618395506b65
Ructar <duckhamm@gmail.com>
2019-03-02 21:57:03 -0500

    changes

--- Old Build/jobs/pool/pool-server.lua
+++ Old Build/jobs/pool/pool-server.lua
@@ -13,14 +13,12 @@ AddEventHandler('pool:getplayerjob', function()
     local source = tonumber(source)
     TriggerEvent('core:getuser', source, function(user)
         local job = user.get("job")
-        if job == 20 then
-            if user.get("wallet") > 500 then
-                user.removeWallet(500)
-                TriggerClientEvent("pool:result", source, job.id)
-            elseif user.get("bank") > 500 then
-                user.removeWallet(500)
-                TriggerClientEvent("pool:result", source, job.id)
-            end
+        if user.get("wallet") > 500 then
+            user.removeWallet(500)
+            TriggerClientEvent("pool:result", source, job.id)
+        elseif user.get("bank") > 500 then
+            user.removeWallet(500)
+            TriggerClientEvent("pool:result", source, job.id)
         end
     end)
 end)
