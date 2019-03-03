6f2c2255736fe57d9cd6c08c24be0712cddec9af
Ructar <duckhamm@gmail.com>
2019-03-02 22:20:16 -0500

    New Cocaine Spot

--- Old Build/others/informer/server.lua
+++ Old Build/others/informer/server.lua
@@ -31,7 +31,7 @@ AddEventHandler("informer:cleancoke", function()
 		local price = 5000
 		if (tonumber(user.get("wallet")) >= tonumber(price)) then
 			user.removeWallet(price)
-			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "top left, scaffolding?? i think? or was it top right" )
+			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "I like to plant seeds in Grapeseed." )
 		else
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Information isn't free, get your bread up!")
 		end
