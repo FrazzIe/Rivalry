7cb997d7f000adea901f4693aef45ae2e7040ea6
skinner904 <skinner904@gmail.com>
2019-04-19 13:45:13 -0400

    increase garbage rent to $250

--- OneSync Build/core_modules/jobs/garbage/server.lua
+++ OneSync Build/core_modules/jobs/garbage/server.lua
@@ -9,11 +9,11 @@ RegisterServerEvent('Garbage.Rent')
 AddEventHandler('Garbage.Rent', function()
 	local source = source
 	TriggerEvent('core:getuser', source, function(user)
-		if user.get("wallet") >= 150 then
-			user.removeWallet(150)
+		if user.get("wallet") >= 250 then
+			user.removeWallet(250)
 			TriggerClientEvent('Garbage.Rent', source)
 		else
-			user.removeBank(150)
+			user.removeBank(250)
 			TriggerClientEvent('Garbage.Rent', source)
 		end
 	end)
