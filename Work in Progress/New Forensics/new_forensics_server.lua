327a6f0fde5ee111441cb629a620a6aa353e6f5e
Ructar <duckhamm@gmail.com>
2019-04-20 23:47:42 -0400

    Update new_forensics_server.lua

--- Work in Progress/New Forensics/new_forensics_server.lua
+++ Work in Progress/New Forensics/new_forensics_server.lua
@@ -66,7 +66,7 @@ AddEventHandler('Forensics.PickUp.Evidence', function(Evidence, Type, Index)
 		RemoveBulletCasing(Evidence)
 	elseif Type == "fingerprint" then
 		RemoveFingerPrint(Index)
-	elseif Type == "DestroyEvidence" then
+	elseif Type == "destroyevidence" then
 		RemoveBulletCasing(Evidence)
 	end
 end)
@@ -115,14 +115,14 @@ end)
 RegisterServerEvent("Forensics.Store")
 AddEventHandler("Forensics.Store", function(Data, Description, Which)
 	local Source = source
-	if Which == "CDS" then
-		exports['GHMattiMySQL']:QueryResult("INSERT INTO police_evidence_cds (`name`,`quantity`,`item_id`,`description`) VALUES (@name,@quantity,@item_id,@description);", { 
+	if Which == "cds" then
+		exports['GHMattiMySQL']:QueryResult("INSERT INTO police_evidence_inventory (`name`,`quantity`,`item_id`,`description`) VALUES (@name,@quantity,@item_id,@description);", { 
 	        ['@name'] = Data.name,
 	        ['@quantity'] = Data.quantity,
 	        ['@item_id'] = Data.item_id,
 	        ['@description'] = Description,
 	    })
-	elseif Which == "Firearm" then
+	elseif Which == "firearm" then
 		exports['GHMattiMySQL']:QueryResult("INSERT INTO police_evidence_firearms (`id`, `character_id`, `sellprice`, `model`, `ammo`, `suppressor`, `flashlight`, `extended_clip`, `scope`, `grip`, `advanced_scope`, `skin`, `owner`, `description`) VALUES (@id, @character_id, @sellprice, @model, @ammo, @suppressor, @flashlight, @extended_clip, @scope, @grip, @advanced_scope, @skin, @owner, @description);", { 
 	        ['@id'] = Data.id,
 	        ['@character_id'] = Data.character_id,
@@ -144,13 +144,13 @@ AddEventHandler("Forensics.Store", function(Data, Description, Which)
 			["@model"] = Data.model,
 			['@description'] = Description,
 		})
-	elseif Which == "Fingerprint" then
+	elseif Which == "fingerprints" then
 		exports['GHMattiMySQL']:QueryResult("INSERT INTO police_evidence_fingerprints (`player`,`licenseplate`,`description`) VALUES (@player,@licenseplate,@description);", { 
 	        ['@player'] = Data.player,
 	        ['@licenseplate'] = Data.licenseplate,
 	        ['@description'] = Description,
 	    })
-	elseif Which == "BulletCasing" then
+	elseif Which == "ballistics" then
 		exports['GHMattiMySQL']:QueryResult("INSERT INTO police_evidence_ballistics (`player`, `weaponused`, `location`, `serialnumber`, `description`) VALUES (@player, @weaponused, @location, @serialnumber, @description);", { 
 	        ['@player'] = Data.player,
 	        ['@weaponused'] = Data.weaponused,
@@ -168,29 +168,33 @@ RegisterServerEvent("Retrieve.Controlled.Substances")
 
 AddEventHandler("Retrieve.Fingerprints", function()
 	local Source = source
-	exports['GHMattiMySQL']:QueryResultAsync("SELECT * FROM police_evidence_fingerprints", {}, function(Fingerprints)
-		TriggerClientEvent("Create.Locker.Fingerprints", Source, Fingerprints)
+	exports['GHMattiMySQL']:QueryResultAsync("SELECT police_evidence_fingerprints.id, police_evidence_fingerprints.player, police_evidence_fingerprints.licenseplate, police_evidence_fingerprints.description, police_evidence_fingerprints.type, UNIX_TIMESTAMP(police_evidence_fingerprints.timestamp) AS timestamp, police_evidence_fingerprints.status FROM police_evidence_fingerprints", {}, function(Fingerprints)
+		local timestamp = os.date("%A %B %d %Y | %I:%M %p", Fingerprints.timestamp)
+		TriggerClientEvent("Create.Locker.Fingerprints", Source, Fingerprints, timestamp)
 	end)
 end)
 
 AddEventHandler("Retrieve.Ballistics", function()
 	local Source = source
-	exports['GHMattiMySQL']:QueryResultAsync("SELECT * FROM police_evidence_ballistics", {}, function(Ballistics)
-		TriggerClientEvent("Create.Locker.Ballistics", Source, Ballistics)
+	exports['GHMattiMySQL']:QueryResultAsync("SELECT police_evidence_ballistics.id, police_evidence_ballistics.player, police_evidence_ballistics.location, police_evidence_ballistics.serialnumber, police_evidence_ballistics.description, police_evidence_ballistics.type, UNIX_TIMESTAMP(police_evidence_ballistics.timestamp) AS timestamp, police_evidence_ballistics.status FROM police_evidence_ballistics", {}, function(Ballistics)
+		local timestamp = os.date("%A %B %d %Y | %I:%M %p", Ballistics.timestamp)
+		TriggerClientEvent("Create.Locker.Ballistics", Source, Ballistics, timestamp)
 	end)
 end)
 
 AddEventHandler("Retrieve.Firearms", function()
 	local Source = source
-	exports['GHMattiMySQL']:QueryResultAsync("SELECT * FROM police_evidence_firearms", {}, function(Firearms)
-		TriggerClientEvent("Create.Locker.Firearms", Source, Firearms)
+	exports['GHMattiMySQL']:QueryResultAsync("SELECT police_evidence_firearms.id, police_evidence_firearms.character_id, police_evidence_firearms.sellprice, police_evidence_firearms.model, police_evidence_firearms.ammo, police_evidence_firearms.suppressor, police_evidence_firearms.flashlight, police_evidence_firearms.extended_clip, police_evidence_firearms.scope, police_evidence_firearms.grip, police_evidence_firearms.advanced_scope, police_evidence_firearms.skin, police_evidence_firearms.owner, police_evidence_firearms.type, UNIX_TIMESTAMP(police_evidence_firearms.timestamp) AS timestamp, police_evidence_firearms.status FROM police_evidence_firearms", {}, function(Firearms)
+		local timestamp = os.date("%A %B %d %Y | %I:%M %p", Firearms.timestamp)
+		TriggerClientEvent("Create.Locker.Firearms", Source, Firearms, timestamp)
 	end)
 end)
 
 AddEventHandler("Retrieve.Controlled.Substances", function()
 	local Source = source
-	exports['GHMattiMySQL']:QueryResultAsync("SELECT * FROM police_evidence_cds", {}, function(ControlledSubstances)
-		TriggerClientEvent("Create.Locker.Controlled.Substances", Source, ControlledSubstances)
+	exports['GHMattiMySQL']:QueryResultAsync("SELECT police_evidence_inventory.id, police_evidence_inventory.character_id, police_evidence_inventory.item_id, police_evidence_inventory.name, police_evidence_inventory.quantity, police_evidence_inventory.canuse, police_evidence_inventory.description, UNIX_TIMESTAMP(police_evidence_inventory.timestamp) AS timestamp, police_evidence_inventory.type FROM police_evidence_inventory", {}, function(ControlledSubstances)
+		local timestamp = os.date("%A %B %d %Y | %I:%M %p", ControlledSubstances.timestamp)
+		TriggerClientEvent("Create.Locker.Controlled.Substances", Source, ControlledSubstances, timestamp)
 	end)
 end)
 
@@ -198,25 +202,26 @@ RegisterServerEvent("Locker.Destroy")
 
 AddEventHandler("Locker.Destroy", function(Data, Which)
 	local Source = source
-	if Which == "Fingerprints" then
+	if Which == "fingerprints" then
 		exports["GHMattiMySQL"]:QueryAsync("DELETE FROM police_evidence_fingerprints WHERE (player=@player) AND (id=@id)", {
 			["@id"] = Data.id,
 			["@player"] = Data.player,
 		})
-	elseif Which == "Ballistics" then
+	elseif Which == "ballistics" then
 		exports["GHMattiMySQL"]:QueryAsync("DELETE FROM police_evidence_ballistics WHERE (player=@player) AND (id=@id)", {
 			["@id"] = Data.id,
 			["@player"] = Data.player,
 		})
-	elseif Which == "Firearms" then
+	elseif Which == "firearms" then
 		exports["GHMattiMySQL"]:QueryAsync("DELETE FROM police_evidence_firearms WHERE (character_id=@character_id) AND (model=@model)", {
 			["@character_id"] = Data.character_id,
 			["@model"] = Data.model,
 		})
-	elseif Which == "CDS" then
-		exports["GHMattiMySQL"]:QueryAsync("DELETE FROM police_evidence_cds WHERE (character_id=@character_id) AND (model=@model)", {
+	elseif Which == "cds" then
+		exports["GHMattiMySQL"]:QueryAsync("DELETE FROM police_evidence_inventory WHERE (item_id=@item_id) AND (id=@id) AND (character_id=@character_id) AND (quantity=@quantity)", {
 			["@item_id"] = Data.item_id,
 			["@id"] = Data.id,
+			["@character_id"] = Data.character_id,
 			['@quantity'] = quantity,
 		})
 	end
@@ -232,7 +237,7 @@ AddEventHandler("Locker.Take", function(Data)
 	elseif Data.type == "firearms" then
 		exports["GHMattiMySQL"]:QueryAsync("UPDATE police_evidence_firearms SET status=@status WHERE (id=@id)", {["@id"] = Data.id, ["@status"] = 1})
 	elseif Data.type == "cds" then
-		exports["GHMattiMySQL"]:QueryAsync("UPDATE police_evidence_cds SET status=@status WHERE (id=@id)", {["@id"] = Data.id, ["@status"] = 1})
+		exports["GHMattiMySQL"]:QueryAsync("UPDATE police_evidence_inventory SET status=@status WHERE (id=@id)", {["@id"] = Data.id, ["@status"] = 1})
 	end
 end)
 
@@ -245,7 +250,7 @@ AddEventHandler("Locker.PutBackEvidence", function(Data)
 	elseif Data.type == "firearms" then
 		exports["GHMattiMySQL"]:QueryAsync("UPDATE police_evidence_firearms SET status=@status WHERE (id=@id)", {["@id"] = Data.id, ["@status"] = 0})
 	elseif Data.type == "cds" then
-		exports["GHMattiMySQL"]:QueryAsync("UPDATE police_evidence_cds SET status=@status WHERE (id=@id)", {["@id"] = Data.id, ["@status"] = 0})
+		exports["GHMattiMySQL"]:QueryAsync("UPDATE police_evidence_inventory SET status=@status WHERE (id=@id)", {["@id"] = Data.id, ["@status"] = 0})
 	end
 end)
 
