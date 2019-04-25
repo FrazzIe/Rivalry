b078b7a07aebe355fe5f053015875ebfd100489c
skinner904 <skinner904@gmail.com>
2019-04-25 04:28:08 -0400

    hunting still too much
    compared to other jobs...

--- OneSync Build/jobs/hunting/client.lua
+++ OneSync Build/jobs/hunting/client.lua
@@ -87,7 +87,7 @@ AddEventHandler('hunting:drawSellViande', function (qte)
 		end
 		if qte > 0 then
 			TriggerEvent('inventory:removeQty',23, 1)
-			local salaire = math.random(75, 150)
+			local salaire = math.random(75, 100)
 			TriggerServerEvent('mission:completed', salaire)
 			ClearPrints()
 			SetTextEntry_2("STRING")
