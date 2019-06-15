edc6501df39ed61ba7689f5d054929e0acf76630
Ructar <duckhamm@gmail.com>
2019-06-14 20:47:59 -0400

    Lots of changes with gunstash

--- Work in Progress/NewProjects/gunstash/client.lua
+++ Work in Progress/NewProjects/gunstash/client.lua
@@ -1,30 +1,42 @@
 local Rivalry = {
 	--Supplier = vector3(),
 	Gangs = {
-		--Russian = vector3(1053.0560302734,-3097.1311035156,-38.999919891357),
-		Italian = vector3(1103.9210205078,-3098.408203125,-38.999961853027),
-		Ballers = vector3(1053.0560302734,-3097.1311035156,-38.999919891357),
+		[1] = vector3(0,0,0),
+		[2] = vector3(1095.060546875,-3098.2133789063,-38.999923706055),
+		[3] = vector3(1053.0560302734,-3097.1311035156,-38.999919891357),
 	},
 	Teleporters = {
 		Italian = {
-			OutsideDoor = vector3(-44.018558502197,-2519.8591308594,7.3945479393005),
-			OutsideDoorHeading = 236.03332519531,
-			InsideDoor = vector3(1087.3448486328,-3099.4184570313,-38.999961853027),
-			InsideDoorHeading = 269.36706542969
+			[1] = {
+				OutsideDoor = vector3(-44.018558502197,-2519.8591308594,7.3945479393005),
+				OutsideDoorHeading = 236.03332519531,
+			},
+			[2] = {
+				InsideDoor = vector3(1087.3448486328,-3099.4184570313,-38.999961853027),
+				InsideDoorHeading = 269.36706542969
+			}
 		},
-		Russian = {
+		-- Russian = {
 
-		},
+		-- },
 		Ballers = {
-		OutsideDoor = vector3(-150.23551940918, -1625.5727539063,36.848297119141),
-		OutsideDoorHeading = 50.321853637695,
-		InsideDoor = vector3(151.41645812988,-1008.0470581055,-99.000022888184),
-		InsideDoorHeading = 1.0677021741867
+			[1] = {
+				OutsideDoor = vector3(-150.23551940918, -1625.5727539063,36.848297119141),
+				OutsideDoorHeading = 50.321853637695,
+			},
+			[2] = {
+				InsideDoor = vector3(151.41645812988,-1008.0470581055,-99.000022888184),
+				InsideDoorHeading = 1.0677021741867
+			}
 		}
 	}
 }
 local OpenMenu = false
+local IsOpened = false
 local Inventory = {}
+Dealer = {}
+Dealer.IsDealer = false
+Dealer.Gang = nil
 
 function DisplayHelpText(Str)
 	BeginTextCommandDisplayHelp("STRING")
@@ -36,59 +48,126 @@ function Notify(Message, Time)
 	exports.pNotify:SendNotification({text = Message or "", type = "error", timeout = Time or 3000, layout = "centerRight", queue = "left"})
 end
 
--- Russian Market
--- Citizen.CreateThread(function()
--- 	while true do
--- 		local Player = PlayerPedId()
--- 		local PlayerPosition = GetEntityCoords(Player, false)
--- 		if #(PlayerPosition - Rivalry.Gangs.Russian) <= 1.0 then
--- 			DisplayHelpText("Press ~INPUT_CONTEXT~ to open stash!")
--- 			if IsControlJustPressed(1, 51) then
--- 				TriggerServerEvent("Open.Stash.Russian")
--- 			end
--- 		end
--- 	end
--- end)
-
--- Italian Market
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		local Player = PlayerPedId()
-		local PlayerPosition = GetEntityCoords(Player, false)
-		if #(PlayerPosition - Rivalry.Gangs.Italian) <= 1.0 then
-			DisplayHelpText("Press ~INPUT_CONTEXT~ to open stash!")
-			if IsControlJustPressed(1, 51) then
-				TriggerServerEvent("Open.Stash.Italian")
-			end
-		end
-	end
-end)
-
--- Ballers Market
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		local Player = PlayerPedId()
-		local PlayerPosition = GetEntityCoords(Player, false)
-		if #(PlayerPosition - Rivalry.Gangs.Ballers) <= 1.0 then
-			DisplayHelpText("Press ~INPUT_CONTEXT~ to open stash!")
-			if IsControlJustPressed(1, 51) then
-				TriggerServerEvent("Open.Stash.Ballers")
-			end
-		end
-	end
+RegisterNetEvent("Stash.Sync")
+AddEventHandler("Stash.Sync", function()
+	IsOpened = true
 end)
 
 RegisterNetEvent("Open.Stash")
-AddEventHandler("Open.Stash", function()
-	print("Opening Menu")
+AddEventHandler("Open.Stash", function(Inv)
+	Inventory = Inv
 	OpenMenu = true
 end)
 
-RegisterNetEvent("Stash.Sync")
-AddEventHandler("Stash.Sync", function(_Inventory)
-	Inventory = _Inventory
+RegisterNetEvent("Close.Stash")
+AddEventHandler("Close.Stash", function()
+	IsOpened = false
+end)
+
+RegisterNetEvent("Dealer:Set")
+AddEventHandler("Dealer:Set", function(_Data, _Dealer, first)
+	Dealer.IsDealer = _Dealer
+
+	if not Dealer.IsDealer then
+		Dealer.Gang = nil
+	else
+		if _Data ~= nil then
+			if _Data.gang ~= nil then
+				Dealer.Gang = _Data.gang
+			end
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if Dealer.IsDealer then
+			local Player = PlayerPedId()
+			local PlayerPosition = GetEntityCoords(Player, false)
+			for Index = 1, #Rivalry.Gangs do
+				if #(PlayerPosition - Rivalry.Gangs[Index]) <= 1.0 then
+					DisplayHelpText("Press ~INPUT_CONTEXT~ to open stash!")
+					if IsControlJustPressed(1, 51) then
+						if not IsOpened then
+							TriggerServerEvent("Open.Stash", Dealer.Gang)
+						else
+							Notify("One person at a time please! For now...", 3100)
+						end
+					end
+				end
+			end
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if Dealer.IsDealer and Dealer.Gang == "italian" then
+			local Player = PlayerPedId()
+			local PlayerPosition = GetEntityCoords(Player, false)
+			for Index = 1, #Rivalry.Teleporters.Italian do
+				if Index == 1 then
+					if #(PlayerPosition - Rivalry.Teleporters.Italian[Index].OutsideDoor) <= 1.0 then
+						DisplayHelpText("Press ~INPUT_CONTEXT~ to enter!")
+						if IsControlJustPressed(1, 51) then
+							TeleportPlayer(Rivalry.Teleporters.Italian[Index + 1].InsideDoor.x, Rivalry.Teleporters.Italian[Index + 1].InsideDoor.y, Rivalry.Teleporters.Italian[Index + 1].InsideDoor.z, Rivalry.Teleporters.Italian[Index + 1].InsideDoorHeading)
+						end
+					end
+				else
+					if #(PlayerPosition - Rivalry.Teleporters.Italian[Index].InsideDoor) <= 1.0 then
+						DisplayHelpText("Press ~INPUT_CONTEXT~ to enter!")
+						if IsControlJustPressed(1, 51) then
+							TeleportPlayer(Rivalry.Teleporters.Italian[Index - 1].OutsideDoor.x, Rivalry.Teleporters.Italian[Index - 1].OutsideDoor.y, Rivalry.Teleporters.Italian[Index - 1].OutsideDoor.z, Rivalry.Teleporters.Italian[Index - 1].OutsideDoorHeading)
+						end
+					end
+				end
+			end
+		end
+		if Dealer.IsDealer and Dealer.Gang == "ballers" then
+			local Player = PlayerPedId()
+			local PlayerPosition = GetEntityCoords(Player, false)
+			for Index = 1, #Rivalry.Teleporters.Ballers do
+				if Index == 1 then
+					if #(PlayerPosition - Rivalry.Teleporters.Ballers[Index].OutsideDoor) <= 1.0 then
+						DisplayHelpText("Press ~INPUT_CONTEXT~ to enter!")
+						if IsControlJustPressed(1, 51) then
+							TeleportPlayer(Rivalry.Teleporters.Ballers[Index + 1].InsideDoor.x, Rivalry.Teleporters.Ballers[Index + 1].InsideDoor.y, Rivalry.Teleporters.Ballers[Index + 1].InsideDoor.z, Rivalry.Teleporters.Ballers[Index + 1].InsideDoorHeading)
+						end
+					end
+				else
+					if #(PlayerPosition - Rivalry.Teleporters.Ballers[Index].InsideDoor) <= 1.0 then
+						DisplayHelpText("Press ~INPUT_CONTEXT~ to enter!")
+						if IsControlJustPressed(1, 51) then
+							TeleportPlayer(Rivalry.Teleporters.Ballers[Index - 1].OutsideDoor.x, Rivalry.Teleporters.Ballers[Index - 1].OutsideDoor.y, Rivalry.Teleporters.Ballers[Index - 1].OutsideDoor.z, Rivalry.Teleporters.Ballers[Index - 1].OutsideDoorHeading)
+						end
+					end
+				end
+			end
+		end
+		-- if Dealer.IsDealer and Dealer.Gang == "russian" then
+		-- 	local Player = PlayerPedId()
+		-- 	local PlayerPosition = GetEntityCoords(Player, false)
+		-- 	for Index = 1, #Rivalry.Teleporters.Russian do
+		-- 		if Index == 1 then
+		-- 			if #(PlayerPosition - Rivalry.Teleporters.Russian[Index].OutsideDoor) <= 1.0 then
+		-- 				DisplayHelpText("Press ~INPUT_CONTEXT~ to enter!")
+		-- 				if IsControlJustPressed(1, 51) then
+		-- 					TeleportPlayer(Rivalry.Teleporters.Russian[Index + 1].InsideDoor.x, Rivalry.Teleporters.Russian[Index + 1].InsideDoor.y, Rivalry.Teleporters.Russian[Index + 1].InsideDoor.z, Rivalry.Teleporters.Russian[Index + 1].InsideDoorHeading)
+		-- 				end
+		-- 			end
+		-- 		else
+		-- 			if #(PlayerPosition - Rivalry.Teleporters.Russian[Index].InsideDoor) <= 1.0 then
+		-- 				DisplayHelpText("Press ~INPUT_CONTEXT~ to enter!")
+		-- 				if IsControlJustPressed(1, 51) then
+		--					TeleportPlayer(Rivalry.Teleporters.Russian[Index - 1].OutsideDoor.x, Rivalry.Teleporters.Russian[Index - 1].OutsideDoor.y, Rivalry.Teleporters.Russian[Index - 1].OutsideDoor.z, Rivalry.Teleporters.Russian[Index - 1].OutsideDoorHeading)
+		-- 				end
+		-- 			end
+		-- 		end
+		-- 	end
+		-- end
+	end
 end)
 
 Citizen.CreateThread(function()
@@ -104,7 +183,7 @@ Citizen.CreateThread(function()
 					WarMenu.SetMenuY("Stash", 0.15)
 					WarMenu.SetTitleBackgroundColor("Stash", 0, 107, 87)
 					WarMenu.CreateSubMenu("Stash_Deposit", "Stash", "DEPOSIT SECTION")
-					WarMenu.CreateSubMenu("Stash_Withdraw", "Stash", "WIRTHDRAW SECTION")
+					WarMenu.CreateSubMenu("Stash_Withdraw", "Stash", "WITHDRAW SECTION")
 					WarMenu.OpenMenu("Stash")
 				else
 					currentItemIndex = 1
@@ -124,26 +203,33 @@ Citizen.CreateThread(function()
 			end
 			if WarMenu.Button("Close") then
 				WarMenu.CloseMenu()
+				TriggerServerEvent("Close.Stash")
+			end
+			if IsControlJustPressed(1, 177) then
+				WarMenu.CloseMenu()
+				TriggerServerEvent("Close.Stash")
 			end
 			WarMenu.Display()
 		end
 		if WarMenu.IsMenuOpened("Stash_Deposit") then
 			for k, v in pairs(user_weapons) do
 				if WarMenu.Button(Weapons_names[k].." ["..v.ammo.."]", "Deposit") then
-					TriggerServerEvent("Stash.Deposit", CurrentGang, v)
+					TriggerServerEvent("Stash.Deposit", user_weapons[k])
+					WarMenu.CloseMenu()
+					TriggerServerEvent("Close.Stash")
 				end
 			end
 			WarMenu.Display()
 		end
 		if WarMenu.IsMenuOpened("Stash_Withdraw") then
-			if Inventory ~= nil then
-				for Index = 1, #Inventory do
-					if WarMenu.Button(Weapons_names[Inventory[Index].model], tostring(Index)) then
-						TriggerServerEvent("Stash.Withdraw", CurrentGang, Index, Inventory[Index])
-					end
+			for k, v in pairs(Inventory) do
+				if WarMenu.Button(Weapons_names[v.model], v.id) then
+					TriggerServerEvent("Stash.Withdraw", v.id, Inventory[k], Dealer.Gang)
+					WarMenu.CloseMenu()
+					TriggerServerEvent("Close.Stash")
 				end
-				WarMenu.Display()
 			end
+			WarMenu.Display()
 		end
 	end
 end)
--- Work in Progress/NewProjects/gunstash/server.lua
+++ Work in Progress/NewProjects/gunstash/server.lua
@@ -1,54 +1,36 @@
-RegisterServerEvent("Open.Stash.Italian")
-AddEventHandler("Open.Stash.Italian", function()
-	local Source = source
-	TriggerEvent("core:getuser", Source, function(user)
-	local CharacterId = user.get("characterID")
-		local result = exports['GHMattiMySQL']:QueryResult("SELECT * FROM gundealer WHERE character_id = @character_id", {["@character_id"] = CharacterId})
-		if result[1] ~= nil then
-			TriggerEvent("Open.Stash", "italian", Source)
-		end
-	end)
-end)
+GunDealers = {}
 
-RegisterServerEvent("Open.Stash.Ballers")
-AddEventHandler("Open.Stash.Ballers", function()
-	local Source = source
-	TriggerEvent("core:getuser", Source, function(user)
-	local CharacterId = user.get("characterID")
-		local result = exports['GHMattiMySQL']:QueryResult("SELECT * FROM gundealer WHERE character_id = @character_id", {["@character_id"] = CharacterId})
-		if result[1] ~= nil then
-			TriggerEvent("Open.Stash", "ballers", Source)
-		end
-	end)
-end)
-
-RegisterServerEvent("Open.Stash.Russian")
-AddEventHandler("Open.Stash.Russian", function()
-	local Source = source
-	TriggerEvent("core:getuser", Source, function(user)
-	local CharacterId = user.get("characterID")
-		local result = exports['GHMattiMySQL']:QueryResult("SELECT * FROM gundealer WHERE character_id = @character_id", {["@character_id"] = CharacterId})
-		if result[1] ~= nil then
-			TriggerEvent("Open.Stash", "russian", Source)
+AddEventHandler("Dealer:Initialise", function(source, identifier, character_id)
+	exports['GHMattiMySQL']:QueryResultAsync("SELECT * FROM gundealer WHERE character_id = @character_id", {["@character_id"] = character_id}, function(result)
+		if result[1] == nil then
+			TriggerClientEvent("Dealer:Set", source, {}, false, true)
+		else
+			GunDealers[source] = result[1]
+			TriggerClientEvent("Dealer:Set", source, GunDealers[source], true, true)
 		end
 	end)
 end)
 
 RegisterServerEvent("Open.Stash")
-AddEventHandler("Open.Stash", function(Gang, Source)
-	exports['GHMattiMySQL']:QueryResultAsync("SELECT * FROM gunstash WHERE ( stash = @stash )", {["@stash"] = Gang}, function(result)
-		if result[1] ~= nil then
-			TriggerClientEvent("Open.Stash", Source, results)
-			TriggerClientEvent("Stash.Sync", -1, results)
-		else
-			TriggerClientEvent("Open.Stash", Source, results)
-			TriggerClientEvent("Stash.Sync", -1, results)
-		end
-	end)
+AddEventHandler("Open.Stash", function(Gang)
+	local Source = source
+	local result = exports['GHMattiMySQL']:QueryResult("SELECT * FROM gunstash WHERE stash = @stash", {["@stash"] = Gang})
+	if #result > 0 then
+		TriggerClientEvent("Stash.Sync", -1)
+		TriggerClientEvent("Open.Stash", Source, result)
+	else
+		TriggerClientEvent("Stash.Sync", -1)
+		TriggerClientEvent("Open.Stash", Source, {})
+	end
+end)
+
+RegisterServerEvent("Close.Stash")
+AddEventHandler("Close.Stash", function()
+	TriggerClientEvent("Close.Stash", -1)
 end)
 
 RegisterServerEvent("Stash.Deposit")
-AddEventHandler("Stash.Deposit", function(Gang, Weapon)
+AddEventHandler("Stash.Deposit", function(Weapon)
 	local Source = source
 	TriggerEvent("core:getuser", Source, function(user)
 		if user_weapons[Source][Weapon.model] then
@@ -59,7 +41,7 @@ AddEventHandler("Stash.Deposit", function(Gang, Weapon)
 			})
 			exports["GHMattiMySQL"]:Insert("gunstash", {
 			    {
-			        ["stash"] = Gang, 
+			        ["stash"] = GunDealers[Source].gang, 
 			        ["model"] = Weapon.model, 
 			        ["ammo"] = Weapon.ammo, 
 			        ["suppressor"] = Weapon.suppressor, 
@@ -80,28 +62,30 @@ AddEventHandler("Stash.Deposit", function(Gang, Weapon)
 end)
 
 RegisterServerEvent("Stash.Withdraw")
-AddEventHandler("Stash.Withdraw", function(Gang, Index, Weapon)
+AddEventHandler("Stash.Withdraw", function(Index, Weapon, Gang)
 	local Source = source
+	local WeaponIndex = Index
+	local CurrentWeapon = Weapon
 	TriggerEvent("core:getuser", Source, function(user)
 		if not user_weapons[Source][Weapon.model] then
-			exports["GHMattiMySQL"]:QueryAsync("DELETE FROM gunstash WHERE (stash=@stash) AND (id=@weapon_id)", {["@stash"] = Gang, ["@weapon_id"] = Index})
+			exports["GHMattiMySQL"]:QueryAsync("DELETE FROM gunstash WHERE stash = @stash AND id = @weapon_id", {["@stash"] = Gang, ["@weapon_id"] = WeaponIndex})
 			exports["GHMattiMySQL"]:Insert("weapons", {
 				{
 					["character_id"] = user.get("characterID"), 
 					["sellprice"] = 0,
-					["model"] = Weapon.model,
-					["ammo"] = Weapon.ammo,
-					["suppressor"] = Weapon.suppressor,
-					["flashlight"] = Weapon.flashlight,
-					["extended_clip"] = Weapon.extended_clip,
-					["scope"] = Weapon.scope,
-					["grip"] = Weapon.grip,
-					["advanced_scope"] = Weapon.advanced_scope,
-					["skin"] = Weapon.skin,
+					["model"] = CurrentWeapon.model,
+					["ammo"] = CurrentWeapon.ammo,
+					["suppressor"] = CurrentWeapon.suppressor,
+					["flashlight"] = CurrentWeapon.flashlight,
+					["extended_clip"] = CurrentWeapon.extended_clip,
+					["scope"] = CurrentWeapon.scope,
+					["grip"] = CurrentWeapon.grip,
+					["advanced_scope"] = CurrentWeapon.advanced_scope,
+					["skin"] = CurrentWeapon.skin,
 					["owner"] = 0,
 				}
-			}, function(Index)
-				user_weapons[Source][Weapon.model] = { id = Index, character_id = user.get("characterID"), sellprice = 0, model = Weapon.model, ammo = Weapon.ammo, suppressor = Weapon.suppressor, flashlight = Weapon.flashlight, extended_clip = Weapon.extended_clip, scope = Weapon.scope, grip = Weapon.grip, advanced_scope = Weapon.advanced_scope, skin = Weapon.skin, owner = 0 }
+			}, function(WeaponIndex)
+				user_weapons[Source][CurrentWeapon.model] = { id = WeaponIndex, character_id = user.get("characterID"), sellprice = 0, model = CurrentWeapon.model, ammo = CurrentWeapon.ammo, suppressor = CurrentWeapon.suppressor, flashlight = CurrentWeapon.flashlight, extended_clip = CurrentWeapon.extended_clip, scope = CurrentWeapon.scope, grip = CurrentWeapon.grip, advanced_scope = CurrentWeapon.advanced_scope, skin = CurrentWeapon.skin, owner = 0 }
 				TriggerClientEvent("weapon:set", Source, user_weapons[Source])
 				TriggerClientEvent("weapon:give", Source)
 				TriggerClientEvent("weapon:sync", -1, user_weapons)
