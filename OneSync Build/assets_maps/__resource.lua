9064f7dbd76a8eebac78aea3e7298a2c96a9f3ab
Ructar <duckhamm@gmail.com>
2019-06-14 23:56:59 -0400

    Italian Interior

--- OneSync Build/assets_maps/__resource.lua
+++ OneSync Build/assets_maps/__resource.lua
@@ -2,6 +2,7 @@ resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
 
 data_file 'DLC_ITYP_REQUEST' 'x64h:/levels/gta5/interiors/int_props/int_medical.rpf/int_medical.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/hei_dlc_heist_police.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ex_int_warehouse_small_dlc.ytyp'
 
 this_is_a_map 'yes'
 
