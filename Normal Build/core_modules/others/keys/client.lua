9518461a53229e53f786f2516fddb98e557d6e09
Ructar <duckhamm@gmail.com>
2019-07-20 15:41:41 -0400

    Fixed Vehicles in LSCustoms | Fixed Keys

--- Normal Build/core_modules/others/keys/client.lua
+++ Normal Build/core_modules/others/keys/client.lua
@@ -171,7 +171,7 @@ Citizen.CreateThread(function()
 		local Player = PlayerPedId()
 		if IsPedSittingInAnyVehicle(Player) then
 			local Vehicle = GetVehiclePedIsIn(Player, false)
-			if not DecorGetBool(Vehicle, "hotwire") and GetPedInVehicleSeat(Vehicle, -1) == Player and not IsVehicleOwned(GetVehicleNumberPlateText(Vehicle)) and GetVehicleClass(Vehicle) ~= 18 and GetVehicleClass(Vehicle) ~= 13 and GetEntityModel(Vehicle) ~= GetHashKey("polmav") and GetEntityModel(Vehicle) ~= GetHashKey("defender") and GetEntityModel(Vehicle) ~= GetHashKey("Predator") then
+			if not DecorGetBool(Vehicle, "hotwire") and GetPedInVehicleSeat(Vehicle, -1) == Player and not IsVehicleOwned(GetVehicleNumberPlateText(Vehicle)) and GetVehicleClass(Vehicle) ~= 18 and GetVehicleClass(Vehicle) ~= 13 and GetEntityModel(Vehicle) ~= GetHashKey("polmav") and GetEntityModel(Vehicle) ~= GetHashKey("defender") and GetEntityModel(Vehicle) ~= GetHashKey("Predator") and not keys_users[GetPlayerServerId(PlayerId())][GetVehicleNumberPlateText(Vehicle)] and string.sub(GetVehicleNumberPlateText(Vehicle), 1, 2) ~= "RR" then
 				DisplayHelpText("Press ~INPUT_CONTEXT~ to try and hotwire this vehicle, or use a hotwire kit from your inventory!")
 				if IsControlJustPressed(1, 51) then
 					TriggerEvent("Hotwire.Car", Player)
