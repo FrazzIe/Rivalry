e9afde850c08f2caa540836dee70f68b2693561d
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-08-14 23:44:08 +0100

    Disable broken 3d /me

--- Normal Build/core_modules2/me/server.lua
+++ Normal Build/core_modules2/me/server.lua
@@ -2,7 +2,8 @@ TriggerEvent("core:addCommand", "me", function(source, args, rawCommand, data)
     local steam = data.get("steam")
     local name = data.get("first_name").." "..data.get("last_name")
 
-    TriggerClientEvent("proximity:msg", -1, source, table.concat(args, " "))
+    TriggerClientEvent("prox_chatMessage", -1, source, name, table.concat(args, " "))
+    --TriggerClientEvent("proximity:msg", -1, source, table.concat(args, " "))
 
     PerformHttpRequest("https://discordapp.com/api/webhooks/611315975052066836/5y7B37qMNxSFvXzabKjb9gqE7spujq1gW5En95JW8RnfLog2iInJYpKULpj_vItnu0VP", function(err, text, headers) end, 'POST', json.encode(
         {
