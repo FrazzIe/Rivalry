e10179f3815875cef98dc2d11202a7ea89a8f3a8
Ructar <duckhamm@gmail.com>
2019-08-15 01:23:13 -0400

    pNotify Changes

--- Normal Build/pNotify/cl_notify.lua
+++ Normal Build/pNotify/cl_notify.lua
@@ -106,40 +106,7 @@ function SetQueueMax(queue, max)
 end
 
 function SendNotification(options)
-    options.animation = options.animation or {}
-    options.sounds = options.sounds or {}
-    options.docTitle = options.docTitle or {}
-
-    local options = {
-        type = options.type or "success",
-        layout = options.layout or "topRight",
-        theme = options.theme or "gta",
-        text = options.text or "Empty Notification",
-        timeout = options.timeout or 5000,
-        progressBar = options.progressBar ~= false and true or false,
-        closeWith = options.closeWith or {},
-        animation = {
-            open = options.animation.open or "gta_effects_open",
-            close = options.animation.close or "gta_effects_close"
-        },
-        sounds = {
-            volume = options.sounds.volume or 1,
-            conditions = options.sounds.conditions or {},
-            sources = options.sounds.sources or {}
-        },
-        docTitle = {
-            conditions = options.docTitle.conditions or {}
-        },
-        modal = options.modal or false,
-        id = options.id or false,
-        force = options.force or false,
-        queue = options.queue or "global",
-        killer = options.killer or false,
-        container = options.container or false,
-        buttons = options.button or false
-    }
-
-    SendNUIMessage({options = options})
+    TriggerEvent("pNotify:SendNotification", options)
 end
 
 RegisterNetEvent("pNotify:SendNotification")
