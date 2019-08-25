87e734e2881d50423d22ddd5792d0665d342645c
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-08-25 18:58:08 +0100

    Add spam protection to /me's

--- Normal Build/core_modules2/me/server.lua
+++ Normal Build/core_modules2/me/server.lua
@@ -1,16 +1,33 @@
+local recentMessages = {}
+
 TriggerEvent("core:addCommand", "me", function(source, args, rawCommand, data)
-    local steam = data.get("steam")
-    local name = data.get("first_name").." "..data.get("last_name")
+    local msg = table.concat(args, " ")
+    local timer = GetGameTimer()
+    local spam = false
 
-    TriggerClientEvent("proximity:msg", -1, source, name, table.concat(args, " "))
+    if recentMessages[source] then
+        if msg == recentMessages[source][1] and timer - recentMessages[source][2] < 5000 then
+            spam = true
+        elseif msg ~= recentMessages[source][1] and timer - recentMessages[source][2] < 500 then
+            spam = true
+        end
+    else
+        recentMessages[source] = { msg, timer }
+    end
 
-    PerformHttpRequest("https://discordapp.com/api/webhooks/611315975052066836/5y7B37qMNxSFvXzabKjb9gqE7spujq1gW5En95JW8RnfLog2iInJYpKULpj_vItnu0VP", function(err, text, headers) end, 'POST', json.encode(
+    if not spam then
+        local steam = data.get("steam")
+        local name = data.get("first_name").." "..data.get("last_name")
+
+        TriggerClientEvent("proximity:msg", -1, source, name, msg)
+
+        PerformHttpRequest("https://discordapp.com/api/webhooks/611315975052066836/5y7B37qMNxSFvXzabKjb9gqE7spujq1gW5En95JW8RnfLog2iInJYpKULpj_vItnu0VP", function(err, text, headers) end, 'POST', json.encode(
         {
             username = "ERP",
             embeds = {
                 {
                     title = name .. " (" .. steam .. ")",
-                    description = table.concat(args, " "),
+                    description = msg,
                     fields = {},
                     color = "2132991",
                 }
@@ -19,4 +36,10 @@ TriggerEvent("core:addCommand", "me", function(source, args, rawCommand, data)
             content = ""
     
         }), { ['Content-Type'] = 'application/json' })
+    end
 end, {help = "Roleplay with text by describing what you are doing."})
+
+AddEventHandler("playerDropped", function()
+    local src = source
+    recentMessages[src] = nil
+end)
