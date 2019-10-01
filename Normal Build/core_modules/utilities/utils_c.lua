5771bb305415efaca8c94ed2ad901d3430b68457
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-10-01 19:45:35 +0100

    update GetClosestPlayer func

--- Normal Build/core_modules/utilities/utils_c.lua
+++ Normal Build/core_modules/utilities/utils_c.lua
@@ -118,19 +118,20 @@ function addBlip(item)
 end
 
 function GetClosestPlayer()
-    local players = GetPlayers()
+    local players = GetActivePlayers()
     local closestDistance = -1
     local closestPlayer = -1
-    local ply = GetPlayerPed(-1)
-    local plyCoords = GetEntityCoords(ply, 0)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed, false)
     
-    for index,value in ipairs(players) do
-        local target = GetPlayerPed(value)
-        if(target ~= ply) then
-            local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
-            local distance = GetDistanceBetweenCoords(targetCoords["x"], targetCoords["y"], targetCoords["z"], plyCoords["x"], plyCoords["y"], plyCoords["z"], true)
-            if(closestDistance == -1 or closestDistance > distance) then
-                closestPlayer = value
+    for i = 1, #players do
+        local targetPed = GetPlayerPed(players[i])
+        if playerPed ~= targetPed then
+            local targetCoords = GetEntityCoords(targetPed, false)
+            local distance = #(playerCoords - targetCoords)
+
+            if closestDistance == -1 or closestDistance > distance then
+                closestPlayer = players[i]
                 closestDistance = distance
             end
         end
