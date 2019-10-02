953fb1a8d5f0a2c39511b6fc850816edac0f5025
Ructar <duckhamm@gmail.com>
2019-10-02 10:02:29 -0400

    More Mechanic Stuff Digiscanner

--- Work in Progress/NewProjects/client.lua
+++ Work in Progress/NewProjects/client.lua
@@ -3,12 +3,14 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0)
 		local Player = PlayerPedId()
 		local PlayerPosition = GetEntityCoords(Player, false)
-		if #(Data.MechanicLocation - PlayerPosition) <= 1.0 then
+		if #(Data.MechanicLocation - PlayerPosition) <= 11.0 then
 			DisplayHelpText("Press ~INPUT_CONTEXT~ to select vehicle!")
 			if IsControlJustPressed(1, 51) then
 				if IsPedInAnyVehicle(Player, true) then
 					SelectedVehicle = GetVehiclePedIsIn(Player, false)
 					TaskPedOutOfVehicle(Player)
+					Citizen.Wait(2000)
+					GivePedWeapon(Player, GetHashKey("WEAPON_DIGISCANNER"))
 				else
 					Notify("You must enter a vehicle prior to starting repairs!")		
 				end
@@ -17,9 +19,15 @@ Citizen.CreateThread(function()
 		if SelectedVehicle ~= nil then
 			local VehiclePosition = GetEntityCoords(SelectedVehicle, false)
 			if #(PlayerPosition - VehiclePosition) <= 2.0 then
-				DisplayHelpText("Press ~INPUT_CONTEXT~ to open repair menu!")
-				if IsControlJustPressed(1, 51) then
-					SendNUIMessage("RepairMenu") 			
+				local CurrentWeapon, HasWeapon = GetCurrentPedWeapon(Player, -1)
+				if CurrentWeapon == GetHashKey("WEAPON_DIGISCANNER") then
+					local Entity = GetEntityPedIsAimingAt(Player)
+					if Entity == SelectedVehicle then
+						DisplayHelpText("Press ~INPUT_CONTEXT~ to open repair menu!")
+						if IsControlJustPressed(1, 51) then
+							SendNUIMessage("RepairMenu") 			
+						end
+					end
 				end
 			end
 		end
