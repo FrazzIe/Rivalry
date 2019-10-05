7c583157326bd219e09687cc0b18c60d31ae86a2
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-10-05 23:47:07 +0100

    Add missing marker

--- Normal Build/core_modules/others/uncuff/client.lua
+++ Normal Build/core_modules/others/uncuff/client.lua
@@ -12,6 +12,7 @@ Citizen.CreateThread(function()
             local distance = #(GetEntityCoords(playerPed) - uncuffLocations[i])
 
             if distance < 20 then
+                RenderMarker(25, uncuffLocations[i].x, uncuffLocations[i].y, uncuffLocations[i].z, 1.5, 1.5, 2.0, 255, 0, 0, 20)
                 if distance < 2 then
                     DisplayHelpText("Press ~INPUT_CONTEXT~ to remove handcuffs for ~r~$~s~".. uncuffCost .. "/~g~$~s~".. (uncuffCost * 3) .. "!")
                     if IsControlJustPressed(0, 51) then
