e1a930f7d5473cdb29e69a4feccc2341210ca7ad
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-10-05 21:49:07 +0100

    buff laundering rates

--- Normal Build/[others]/others-laundering/server.lua
+++ Normal Build/[others]/others-laundering/server.lua
@@ -5,11 +5,11 @@ AddEventHandler("dm:clean", function(distance)
 	TriggerEvent("core:getuser", source, function(user)
 		local dirty = user.get("dirty")
 		if distance < 500 then
-			payment = math.random(100, 500) + (distance/5)
-		elseif distance > 500 and distance < 1000 then
 			payment = math.random(500, 1000) + (distance/5)
-		else
+		elseif distance > 500 and distance < 1000 then
 			payment = math.random(1000, 1500) + (distance/5)
+		else
+			payment = math.random(1500, 2000) + (distance/5)
 		end
 		if dirty >= payment and dirty > 0 then
 			user.addWallet(payment)
