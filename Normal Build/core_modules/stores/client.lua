e548702e6c6646bcc9afc503ded5c74cc722297a
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-10-15 17:52:28 +0100

    add ems shops to paleto and sandy hospitals

--- Normal Build/core_modules/stores/client.lua
+++ Normal Build/core_modules/stores/client.lua
@@ -139,8 +139,11 @@ ToolsBlips = {
 	{ x = -11.827798843384, y = 6500.1225585938, z = 31.498636245728}
 }
 
-EMSShop = vector3(338.09140014648,-585.62628173828,28.791477203369)
-
+EMSShops = {
+	vector3(338.09140014648,-585.62628173828,28.791477203369), -- Pillbox Lower
+	vector3(1820.7316894531, 3674.8110351563, 34.270053863525), -- Sandy Shores Medical Center
+	vector3(-249.94438171387, 6325.3369140625, 32.43399810791), -- Paleto Bay Medical Center
+}
 stores2 = {
 	normal = {
 	    {name="Convenience store", sprite=52, x=1961.1140136719, y=3741.4494628906,z=32.34375 },
@@ -337,73 +340,76 @@ Citizen.CreateThread(function()
 		if exports.emsjob:getIsInService() then
 			local Player = PlayerPedId()
 			local PlayerPosition = GetEntityCoords(Player, false)
-			if #(PlayerPosition - EMSShop) <= 10 then
-				RenderMarker(25, EMSShop.x, EMSShop.y, EMSShop.z, 1.5, 1.5, 2.0, 255, 255, 0, 20)
-				if #(PlayerPosition - EMSShop) <= 1 then
-					DisplayHelpText("Press ~INPUT_CONTEXT to open medical closet!")
-					if IsControlJustPressed(1, 51) then
-						if not WarMenu.IsMenuOpened("MedicalStore") then
-							if not WarMenu.DoesMenuExist("MedicalStore") then
-								WarMenu.CreateMenu("MedicalStore", "Department Store")
-								WarMenu.SetSpriteTitle("MedicalStore", "shopui_title_clubhousemod")
-								WarMenu.SetSubTitle("MedicalStore", "CATEGORIES")
-								WarMenu.SetMenuX("MedicalStore", 0.6)
-								WarMenu.SetMenuY("MedicalStore", 0.15)
-								WarMenu.SetTitleBackgroundColor("MedicalStore", 0, 107, 87)
-								for k,v in pairs(EMSMarket) do
-									WarMenu.CreateSubMenu(v.Category, "MedicalStore", v.Category.." SECTION")
-									for i,j in pairs(v.Items) do
-										WarMenu.CreateSubMenu(j.Name, v.Category, j.Name)
+
+			for i = 1, #EMSShops do
+				if #(PlayerPosition - EMSShops[i]) <= 10 then
+					RenderMarker(25, EMSShops[i].x, EMSShops[i].y, EMSShops[i].z, 1.5, 1.5, 2.0, 255, 255, 0, 20)
+					if #(PlayerPosition - EMSShops[i]) <= 1 then
+						DisplayHelpText("Press ~INPUT_CONTEXT to open medical closet!")
+						if IsControlJustPressed(1, 51) then
+							if not WarMenu.IsMenuOpened("MedicalStore") then
+								if not WarMenu.DoesMenuExist("MedicalStore") then
+									WarMenu.CreateMenu("MedicalStore", "Department Store")
+									WarMenu.SetSpriteTitle("MedicalStore", "shopui_title_clubhousemod")
+									WarMenu.SetSubTitle("MedicalStore", "CATEGORIES")
+									WarMenu.SetMenuX("MedicalStore", 0.6)
+									WarMenu.SetMenuY("MedicalStore", 0.15)
+									WarMenu.SetTitleBackgroundColor("MedicalStore", 0, 107, 87)
+									for k,v in pairs(EMSMarket) do
+										WarMenu.CreateSubMenu(v.Category, "MedicalStore", v.Category.." SECTION")
+										for i,j in pairs(v.Items) do
+											WarMenu.CreateSubMenu(j.Name, v.Category, j.Name)
+										end
 									end
-								end
-								WarMenu.OpenMenu("MedicalStore")
-							else
-								currentItemIndex = 1
-								WarMenu.OpenMenu("MedicalStore")
-							end
-						else
-							WarMenu.CloseMenu()
-						end		
-					end
-					if WarMenu.IsMenuOpened("MedicalStore") then
-						for k,v in pairs(EMSMarket) do
-							WarMenu.MenuButton(v.Category, v.Category)
-						end
-						if WarMenu.Button("Close") then
-							WarMenu.CloseMenu()
-						end
-						WarMenu.Display()
-					end
-					for k,v in pairs(EMSMarket) do
-						if WarMenu.IsMenuOpened(v.Category) then
-							for i,j in pairs(v.Items) do
-								if WarMenu.MenuButton(j.Name, j.Name) then
+									WarMenu.OpenMenu("MedicalStore")
+								else
 									currentItemIndex = 1
+									WarMenu.OpenMenu("MedicalStore")
 								end
+							else
+								WarMenu.CloseMenu()
+							end		
+						end
+						if WarMenu.IsMenuOpened("MedicalStore") then
+							for k,v in pairs(EMSMarket) do
+								WarMenu.MenuButton(v.Category, v.Category)
+							end
+							if WarMenu.Button("Close") then
+								WarMenu.CloseMenu()
 							end
 							WarMenu.Display()
 						end
-					end
-					for k,v in pairs(EMSMarket) do
-						for i,j in pairs(v.Items) do
-							if WarMenu.IsMenuOpened(j.Name) then
-								if WarMenu.Button("Buy "..currentItemIndex.." "..j.Name.."(s)", "$"..j.Cost*currentItemIndex) then
-									TriggerServerEvent("item:buy", j.Id, currentItemIndex)
-								end
-								if WarMenu.ComboBox("Quantity", j.Quantity, currentItemIndex, selectedItemIndex, function(currentIndex, selectedIndex)
-									currentItemIndex = currentIndex
-									selectedItemIndex = selectedIndex
-								end) then
+						for k,v in pairs(EMSMarket) do
+							if WarMenu.IsMenuOpened(v.Category) then
+								for i,j in pairs(v.Items) do
+									if WarMenu.MenuButton(j.Name, j.Name) then
+										currentItemIndex = 1
+									end
 								end
 								WarMenu.Display()
 							end
 						end
+						for k,v in pairs(EMSMarket) do
+							for i,j in pairs(v.Items) do
+								if WarMenu.IsMenuOpened(j.Name) then
+									if WarMenu.Button("Buy "..currentItemIndex.." "..j.Name.."(s)", "$"..j.Cost*currentItemIndex) then
+										TriggerServerEvent("item:buy", j.Id, currentItemIndex)
+									end
+									if WarMenu.ComboBox("Quantity", j.Quantity, currentItemIndex, selectedItemIndex, function(currentIndex, selectedIndex)
+										currentItemIndex = currentIndex
+										selectedItemIndex = selectedIndex
+									end) then
+									end
+									WarMenu.Display()
+								end
+							end
+						end
+					elseif #(PlayerPosition - EMSShops[i]) > 1.0 then
+						if WarMenu.IsMenuOpened("MedicalStore") then
+							WarMenu.CloseMenu()
+						end
 					end
-                elseif #(PlayerPosition - EMSShop) > 1.0 then
-                	if WarMenu.IsMenuOpened("MedicalStore") then
-                		WarMenu.CloseMenu()
-                	end
-                end
+				end
 			end
 		end
 	end
