dcf5e27b7f26271fd982826e093635e787707e0f
skinner904 <skinner904@gmail.com>
2020-01-16 15:12:57 -0500

    informer wants dirty cash

--- OneSync Build/[others]/others-informer/server.lua
+++ OneSync Build/[others]/others-informer/server.lua
@@ -3,7 +3,7 @@ AddEventHandler("informer:getweed", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 3000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 		user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "I heard it's hidden in the mountains near a freeway protected by a herd of cows." )
 		else
@@ -16,7 +16,7 @@ AddEventHandler("informer:dirtycoke", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 5000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Some mail company building up north." )
 		else
@@ -29,7 +29,7 @@ AddEventHandler("informer:cleancoke", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 5000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "The cops found the lab so we had to move again.. Look for Stoner Cement Works." )
 		else
@@ -42,7 +42,7 @@ AddEventHandler("informer:convertweed", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 5000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Make a phone call." )
 		else
@@ -55,7 +55,7 @@ AddEventHandler("informer:sellweed", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 5000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Umm.. try talking to other human beings you basement dweller." )
 		else
@@ -68,7 +68,7 @@ AddEventHandler("informer:sellweedbulk", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 10000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Near the Smoke in the Water, there's an all green building, the buyer lives in one of the apartments" )
 		else
@@ -81,7 +81,7 @@ AddEventHandler("informer:getmeth", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 15000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Vehicle: Jesse and Heisenberg use one of these, you may need one and get to Grapeseed!." )
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "A Meth lab would probably be a great place to start looking for chemicals to produce Meth")
@@ -95,7 +95,7 @@ AddEventHandler("informer:producemeth", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 15000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 		user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Erm..Meth heads cook at a dock, off the Great Ocean Highway." )
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "When looking at the map, just North, west of the Chiliad Mountains")
@@ -109,7 +109,7 @@ AddEventHandler("informer:sellmeth", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 15000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 		user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Pretty sure a good place is El Burro Heights? there is a abandon warehouse, you can't miss it!" )
 		else
@@ -122,7 +122,7 @@ AddEventHandler("informer:getguns", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 10000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 		user.removeWallet(price)
 		TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "This might be a painful find, but somewhere in the southern part of Los Santos." )
 		else
@@ -135,7 +135,7 @@ AddEventHandler("informer:getlaunder", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 4000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 		user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Try your luck at the Casino." )
 		else
@@ -149,7 +149,7 @@ AddEventHandler("informer:resthouse", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 3000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "A field medic from Fort Zancudo recently setup shop near his base. Check out the back room clinic for all of your shadier medical needs." )
 		else
@@ -163,7 +163,7 @@ AddEventHandler("informer:chopshop", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 2000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "I heard there is a old recycling place out on Route 68! As well there is a old garage in Downtown Los Santos. Both places have been scraping cars illegaly!" )
 		else
@@ -177,7 +177,7 @@ AddEventHandler("informer:uncuff", function()
 	local source = tonumber(source)
 	TriggerEvent('core:getuser', source, function(user)
 		local price = 1000
-		if (tonumber(user.get("wallet")) >= tonumber(price)) then
+		if (tonumber(user.get("dirty")) >= tonumber(price)) then
 			user.removeWallet(price)
 			TriggerClientEvent('chatMessage', source, 'Informer ', {0, 255, 100}, "Look for this sign: SE RENTA (323) 555-0103. Good luck!" )
 		else
