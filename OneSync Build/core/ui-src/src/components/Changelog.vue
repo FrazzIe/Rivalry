d811ed404dee372c268a27dbe86d1bca08723b3c
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-02-18 22:28:31 +0000

    add toolbars to components

--- OneSync Build/core/ui-src/src/components/Changelog.vue
+++ OneSync Build/core/ui-src/src/components/Changelog.vue
@@ -1,6 +1,17 @@
 <template>
 	<v-flex xs4>
 		<v-card class="elevation-12 css-content" tile>
+			<v-toolbar dense color="primary" dark flat card height="45">
+				<v-toolbar-title>Changelog</v-toolbar-title>
+					<v-spacer></v-spacer>
+
+					<v-tooltip right>
+						<v-btn flat icon slot="activator" @click="">
+							<v-icon>refresh</v-icon>
+						</v-btn>
+						<span>Refresh</span>
+					</v-tooltip>
+			</v-toolbar>
 		</v-card>
 	</v-flex>
 </template>
--- OneSync Build/core/ui-src/src/components/Character.vue
+++ OneSync Build/core/ui-src/src/components/Character.vue
@@ -1,6 +1,18 @@
 <template>
 	<v-flex xs8>
 		<v-card class="elevation-12 css-content" tile>
+			<v-toolbar dense color="primary" dark flat card height="45">
+				<v-toolbar-title>Character selection</v-toolbar-title>
+
+					<v-spacer></v-spacer>
+
+					<v-tooltip right>
+						<v-btn flat icon slot="activator" @click="">
+							<v-icon>add</v-icon>
+						</v-btn>
+						<span>New character</span>
+					</v-tooltip>
+			</v-toolbar>
 		</v-card>
 	</v-flex>
 </template>
