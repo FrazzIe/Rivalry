5190bc4b395a9cb1e2a45ee6ef5e4a68825962eb
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-02-18 20:53:10 +0000

    init vue app

---
+++ OneSync Build/core/ui-src/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
