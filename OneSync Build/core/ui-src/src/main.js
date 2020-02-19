83ba8599964da77a7452902e5b405962dae3f99a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-02-19 18:29:04 +0000

    add vee-validate for form validation

--- OneSync Build/core/ui-src/src/main.js
+++ OneSync Build/core/ui-src/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import VeeValidate from 'vee-validate';
 import App from './App.vue';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
+Vue.use(VeeValidate);
 
 Vue.config.productionTip = false
 
