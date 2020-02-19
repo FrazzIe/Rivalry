d3fb1b9db5a2e2a6c5bc18f0ff40e2766edf2683
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-02-19 01:54:56 +0000

    add roboto font

--- OneSync Build/core/ui-src/src/main.js
+++ OneSync Build/core/ui-src/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import App from './App.vue';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
